8ff8d4dd4aaeb449563972b9d202cfb90661c3af
Raúl Yeguas <neokore@gmail.com>
2020-11-19 13:12:10 +0100

    Enable Storybook

--- src/ui/.storybook/main.js
+++ src/ui/.storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/preset-create-react-app',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
   ],
