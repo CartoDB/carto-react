d9c972d11d24473840de1a4310297b304b0363b8
VictorVelarde <victor.velarde@gmail.com>
2020-11-20 19:31:16 +0100

    fix: add regenerator runtime

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ const config = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime']
         }
      }
     ]
