84946ea9bcb0c84dee7327fb5424883833569647
Raúl Yeguas <neokore@gmail.com>
2020-12-18 09:44:44 +0100

    Mobility and GMaps new design (#59)
    * Layout adjustments for mobility and GMaps
    
    * Remove StreetView control from GMaps
    
    * Make Mapbox attrib bubble wider
    
    * Remove uppercase alert messages
    
    * Update changelog

--- src/ui/theme/carto-theme.js
+++ src/ui/theme/carto-theme.js
@@ -428,14 +428,21 @@ export const cartoThemeOptions = {
           },
           
           '&.mapboxgl-compact': {
-            maxWidth: '75%',
             backgroundColor: 'transparent',
+            right: spacing(0.5),
+            bottom: spacing(2.5),
+
+            // Mobile
+            '@media (max-width: 600px)': {
+              bottom: spacing(0.5),
+            },
 
             '& .mapboxgl-ctrl-attrib-button': {
               backgroundImage: `url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20height='24'%20viewBox='0%200%2024%2024'%20width='24'%3E%3Cg%3E%3Crect%20fill='none'%20height='24'%20width='24'%20x='0'/%3E%3C/g%3E%3Cg%3E%3Cg%3E%3Cg%3E%3Cpath%20d='M11.88,9.14c1.28,0.06,1.61,1.15,1.63,1.66h1.79c-0.08-1.98-1.49-3.19-3.45-3.19C9.64,7.61,8,9,8,12.14%20c0,1.94,0.93,4.24,3.84,4.24c2.22,0,3.41-1.65,3.44-2.95h-1.79c-0.03,0.59-0.45,1.38-1.63,1.44C10.55,14.83,10,13.81,10,12.14%20C10,9.25,11.28,9.16,11.88,9.14z%20M12,2C6.48,2,2,6.48,2,12s4.48,10,10,10s10-4.48,10-10S17.52,2,12,2z%20M12,20c-4.41,0-8-3.59-8-8%20s3.59-8,8-8s8,3.59,8,8S16.41,20,12,20z'%20fill='${variables.palette.text.secondary}'/%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
               backgroundColor: 'rgba(255,255,255,.8)',
               top: 'auto',
               bottom: 0,
+              right: 0,
 
               '&:not(:disabled):hover': {
                 backgroundColor: 'rgba(255,255,255,.8)'
