364c8347e8961287071f624336122982ca92fb82
AdriSolid <biotekgis@gmail.com>
2021-01-28 17:46:28 +0100

    chore: integrate tests tooling

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,7 @@ const config = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-transform-runtime']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
