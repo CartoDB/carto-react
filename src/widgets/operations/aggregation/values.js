f917ff97dc77b7899f7baa67999e85175f4fdb77
AdriSolid <biotekgis@gmail.com>
2021-02-01 15:20:45 +0100

    fix: find min-max values for both features & values

--- src/widgets/operations/aggregation/values.js
+++ src/widgets/operations/aggregation/values.js
@@ -1,15 +1,28 @@
 import { AggregationTypes } from '../../AggregationTypes';
 
+const isFeature = (val) => typeof val === 'object';
+
 const sum = (v, column) =>
-  v.reduce(
-    (a, b) => (typeof b === 'object' ? a + (b.properties[column] || 0) : a + b),
-    0
-  );
+  v.reduce((a, b) => (isFeature(b) ? a + (b.properties[column] || 0) : a + b), 0);
+
+const findMinOrMaxValue = (type, val, column) => {
+  const infinityWithSignus = type === AggregationTypes.MAX ? -Infinity : Infinity;
+
+  // features
+  if (isFeature(val[0]) && column) {
+    return Math[type](...val.map((v) => v.properties[column] || 0), infinityWithSignus);
+  }
+
+  // values
+  return val.reduce((a, b) => Math[type](a, b), infinityWithSignus);
+};
 
 export const aggregationFunctions = {
   [AggregationTypes.COUNT]: (val) => val.length,
-  [AggregationTypes.MIN]: (val) => val.reduce((a, b) => Math.min(a, b), Infinity),
-  [AggregationTypes.MAX]: (val) => val.reduce((a, b) => Math.max(a, b), -Infinity),
+  [AggregationTypes.MIN]: (val, column) =>
+    findMinOrMaxValue(AggregationTypes.MIN, val, column),
+  [AggregationTypes.MAX]: (val, column) =>
+    findMinOrMaxValue(AggregationTypes.MAX, val, column),
   [AggregationTypes.SUM]: (val, column) => sum(val, column),
   [AggregationTypes.AVG]: (val, column) => sum(val, column) / val.length
 };
