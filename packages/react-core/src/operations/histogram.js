3ebc45da253dc1ea38b2c5cb229ec85e1074f7a7
Javier Aragón <javier.aragon@geographica.gs>
2021-03-09 11:26:47 +0100

    refactor

---
+++ packages/react-core/src/operations/histogram.js
@@ -0,0 +1,43 @@
+import { aggregationFunctions } from './aggregation/values';
+
+export function histogram(features, columnName, ticks, operation) {
+  if (Array.isArray(features) && features.length === 0) {
+    return [];
+  }
+
+  ticks = [Number.MIN_SAFE_INTEGER, ...ticks, Number.MAX_SAFE_INTEGER];
+
+  const binsContainer = ticks.map((tick, idx, arr) => ({
+    bin: idx,
+    start: tick,
+    end: arr[idx + 1],
+    values: []
+  }));
+
+  features.forEach((feature) => {
+    const featureValue = feature[columnName];
+
+    if (!featureValue) {
+      return;
+    }
+
+    const binContainer = binsContainer.find(
+      (bin) => bin.start <= featureValue && bin.end > featureValue
+    );
+
+    if (!binContainer) {
+      return;
+    }
+
+    binContainer.values.push(featureValue);
+  });
+
+  const targetOperation = aggregationFunctions[operation];
+
+  if (targetOperation) {
+    const transformedBins = binsContainer.map((binContainer) => binContainer.values);
+    return transformedBins.map((values) => (values.length ? targetOperation(values) : 0));
+  }
+
+  return [];
+}
