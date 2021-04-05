862ff85b9902031b251487dcbb2e295b44d66bff
AdriSolid <biotekgis@gmail.com>
2021-04-05 18:34:38 +0200

    remove unused code

--- packages/react-api/src/index.d.ts
+++ packages/react-api/src/index.d.ts
@@ -2,4 +2,3 @@ export { executeSQL } from './api/SQL';
 export { SourceTypes } from './api/SourceTypes';
 
 export { default as useCartoLayerProps } from './hooks/useCartoLayerProps';
-export { default as useWindowSize } from './hooks/useWindowSize';
