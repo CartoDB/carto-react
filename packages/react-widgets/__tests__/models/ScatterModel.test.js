af983dea97525ffa465e0e4bd5e69af7388966ac
Víctor Velarde <victor.velarde@gmail.com>
2021-06-08 20:56:21 +0200

    Remove SQL queries from ScatterPlotModel
    This way it will work the same as the rest of models (eg HistogramModel), without a dependency on SQL API and working purely based on viewportFeatures

--- packages/react-widgets/__tests__/models/ScatterModel.test.js
+++ packages/react-widgets/__tests__/models/ScatterModel.test.js
@@ -1,10 +1,5 @@
-import { minify } from 'pgsql-minify';
-
-import { getScatter, buildSqlQueryToGetScatter } from '../../src/models/ScatterPlotModel';
-import { SourceTypes } from '@carto/react-api';
+import { getScatter } from '../../src/models/ScatterPlotModel';
 import { Methods, executeTask } from '@carto/react-workers';
-
-import { mockSqlApiRequest, mockClear } from '../mockSqlApiRequest';
 
 jest.mock('@carto/react-workers', () => ({
   executeTask: jest.fn(),
@@ -35,83 +30,46 @@ describe('getScatter', () => {
     );
   });
 
-  test('should throw if using CartoBQTilerLayer without viewportFilter', async () => {
-    await expect(
-      getScatter({ type: SourceTypes.BIGQUERY, viewportFilter: false })
-    ).rejects.toThrow(
-      'Scatter Widget error: BigQuery layer needs "viewportFilter" prop set to true.'
-    );
-  });
-
-  describe('SQL Layer', () => {
-    describe('should execute a SqlApi request when using "viewportFilter": false', () => {
-      const response = { rows: features('x', 'y') };
-      const sql = 'SELECT * FROM retail_stores LIMIT 4';
-      const credentials = {
-        username: 'public',
-        apiKey: 'default_public',
-        serverUrlTemplate: 'https://{user}.carto.com'
-      };
-
-      mockSqlApiRequest({ response, sql, credentials });
-
-      beforeEach(() => {
-        mockClear();
-      });
-
-      test('should call SqlApi', async () => {
-        const params = {
-          data: sql,
-          credentials,
-          type: SourceTypes.SQL,
-          xAxisColumn: 'x',
-          yAxisColumn: 'y',
-          viewportFilter: false
-        };
-        const scatterData = await getScatter(params);
-        const values = features('x', 'y').map((f) => [f.x, f.y]);
-        expect(scatterData).toEqual(values);
-      });
-    });
-  });
-  test('should filter viewport features - "viewportFilter" prop is true', async () => {
-    const viewportFeatures = features('x', 'y');
-    const values = viewportFeatures.map((f) => [f.x, f.y]);
-    executeTask.mockImplementation(() => Promise.resolve(values));
-    const params = {
-      type: SourceTypes.SQL,
+  describe('should correctly handle viewport features', () => {
+    const scatterPlotParams = {
       xAxisColumn: 'x',
       yAxisColumn: 'y',
-      viewportFilter: true,
-      viewportFeatures
+      filters: {},
+      dataSource: 'whatever-data-source'
     };
-    const scatterData = await getScatter(params);
-    expect(scatterData).toEqual(values);
+
+    test('correctly returns data', async () => {
+      const viewportFeatures = features('x', 'y');
+      const values = viewportFeatures.map((f) => [f.x, f.y]);
+      executeTask.mockImplementation(() => Promise.resolve(values));
+
+      const histogram = await getScatter(scatterPlotParams);
+      expect(histogram).toEqual(values);
+    });
+
+    test('correctly called', async () => {
+      const { filters, xAxisColumn, yAxisColumn, dataSource } = scatterPlotParams;
+      await getScatter(scatterPlotParams);
+      expect(executeTask).toHaveBeenCalledWith(
+        dataSource,
+        Methods.VIEWPORT_FEATURES_SCATTERPLOT,
+        { filters, xAxisColumn, yAxisColumn }
+      );
+    });
   });
-});
 
-test('buildSqlQueryToGetScatter', () => {
-  const sql = 'SELECT * FROM retail_stores LIMIT 4';
-  const credentials = {
-    username: 'public',
-    apiKey: 'default_public',
-    serverUrlTemplate: 'https://{user}.carto.com'
-  };
-  const params = {
-    data: sql,
-    credentials,
-    type: SourceTypes.SQL,
-    xAxisColumn: 'x',
-    yAxisColumn: 'y',
-    viewportFilter: false
-  };
-
-  const query = `
-    SELECT 
-      ${params.xAxisColumn}, ${params.yAxisColumn}
-    FROM 
-      (${params.data}) as q1
-  `;
-
-  expect(buildSqlQueryToGetScatter(params)).toEqual(minify(query));
+  // test('should filter viewport features - "viewportFilter" prop is true', async () => {
+  //   const viewportFeatures = features('x', 'y');
+  //   const values = viewportFeatures.map((f) => [f.x, f.y]);
+  //   executeTask.mockImplementation(() => Promise.resolve(values));
+  //   const params = {
+  //     type: SourceTypes.SQL,
+  //     xAxisColumn: 'x',
+  //     yAxisColumn: 'y',
+  //     viewportFilter: true,
+  //     viewportFeatures
+  //   };
+  //   const scatterData = await getScatter(params);
+  //   expect(scatterData).toEqual(values);
+  // });
 });
--- packages/react-widgets/src/models/ScatterPlotModel.js
+++ packages/react-widgets/src/models/ScatterPlotModel.js
@@ -1,62 +1,15 @@
-import { minify } from 'pgsql-minify';
-import {
-  _buildFeatureFilter as buildFeatureFilter,
-  _filtersToSQL as filtersToSQL,
-  scatterPlot
-} from '@carto/react-core';
-import { executeSQL, SourceTypes } from '@carto/react-api';
 import { Methods, executeTask } from '@carto/react-workers';
 
 export const getScatter = async (props) => {
-  const {
-    data,
-    credentials,
-    xAxisColumn,
-    yAxisColumn,
-    filters,
-    opts,
-    viewportFilter,
-    dataSource,
-    type
-  } = props;
+  const { data, xAxisColumn, yAxisColumn, filters, dataSource } = props;
+
   if (Array.isArray(data)) {
     throw new Error('Array is not a valid type to get scatter plot');
   }
-  if (type === SourceTypes.BIGQUERY && !viewportFilter) {
-    throw new Error(
-      'Scatter Widget error: BigQuery layer needs "viewportFilter" prop set to true.'
-    );
-  }
-  if (viewportFilter) {
-    return executeTask(dataSource, Methods.VIEWPORT_FEATURES_SCATTERPLOT, {
-      filters,
-      xAxisColumn,
-      yAxisColumn
-    });
-  }
-  const query = buildSqlQueryToGetScatter({
-    data,
+
+  return executeTask(dataSource, Methods.VIEWPORT_FEATURES_SCATTERPLOT, {
+    filters,
     xAxisColumn,
-    yAxisColumn,
-    filters
+    yAxisColumn
   });
-  const queryResult = await executeSQL(credentials, query, opts);
-  const result = scatterPlot(queryResult, xAxisColumn, yAxisColumn);
-  return result;
-};
-
-export const buildSqlQueryToGetScatter = ({
-  data,
-  xAxisColumn,
-  yAxisColumn,
-  filters
-}) => {
-  const query = `
-    SELECT 
-      ${xAxisColumn}, ${yAxisColumn}
-    FROM (${data}) as q1
-    ${filtersToSQL(filters)}
-  `;
-
-  return minify(query);
 };
