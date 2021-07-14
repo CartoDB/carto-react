19994166571fd27105a98d53c4586002a4e5129e
Alberto Arana <aaranadev@gmail.com>
2021-07-14 17:09:42 +0200

    removed _ export

--- packages/react-core/src/index.d.ts
+++ packages/react-core/src/index.d.ts
@@ -17,12 +17,6 @@ export { groupValuesByColumn } from './operations/groupby';
 export { histogram } from './operations/histogram';
 export { scatterPlot } from './operations/scatterPlot';
 
-export {
-  FilterTypes as _FilterTypes,
-  filtersToSQL as _filtersToSQL,
-  getApplicableFilters as _getApplicableFilters
-} from './filters/FilterQueryBuilder';
-export { buildFeatureFilter as _buildFeatureFilter } from './filters/Filter';
 export { viewportFeatures } from './filters/viewportFeatures';
 export { viewportFeaturesBinary } from './filters/viewportFeaturesBinary';
 export { viewportFeaturesGeoJSON } from './filters/viewportFeaturesGeoJSON';
