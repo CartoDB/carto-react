da3eb89b28425133b99c9a68f841c93696a1c918
Víctor Velarde <victor.velarde@gmail.com>
2021-08-03 17:22:16 +0200

    Use Carto preffix when naming credentials types

--- packages/react-api/src/types.d.ts
+++ packages/react-api/src/types.d.ts
@@ -2,41 +2,41 @@ import { DataFilterExtension } from '@deck.gl/extensions';
 import { MAP_TYPES, API_VERSIONS } from '@deck.gl/carto';
 import { FeatureCollection } from 'geojson';
 
-interface Credentials2 {
-  apiVersion: API_VERSIONS.V1 | API_VERSIONS.V2,
-  username: string,
-  apiKey: string,
-  region?: string,
-  mapsUrl?: string,
-  serverUrlTemplate?: string
+interface CredentialsCarto2 {
+  apiVersion: API_VERSIONS.V1 | API_VERSIONS.V2;
+  username: string;
+  apiKey: string;
+  region?: string;
+  mapsUrl?: string;
+  serverUrlTemplate?: string;
 }
 
-interface Credentials3 {
-  apiVersion?: API_VERSIONS.V3,
-  apiBaseUrl?: string,
-  accessToken?: string,
+interface CredentialsCarto3 {
+  apiVersion?: API_VERSIONS.V3;
+  apiBaseUrl?: string;
+  accessToken?: string;
 }
 
-export type Credentials = Credentials2 | Credentials3;
+export type Credentials = CredentialsCarto2 | CredentialsCarto3;
 
 export type SourceProps = {
-  data: string,
-  type: MAP_TYPES.QUERY | MAP_TYPES.TABLE | MAP_TYPES.TILESET
-  connection?: string,
-  credentials: Credentials
-}
+  data: string;
+  type: MAP_TYPES.QUERY | MAP_TYPES.TABLE | MAP_TYPES.TILESET;
+  connection?: string;
+  credentials: Credentials;
+};
 
 export type UseCartoLayerFilterProps = {
-  binary?: boolean,
-  uniqueIdProperty?: string,
-  onViewportLoad?: Function,
-  onDataLoad?: Function
-  getFilterValue: Function,
-  filterRange: [number, number],
-  extensions: DataFilterExtension[],
+  binary?: boolean;
+  uniqueIdProperty?: string;
+  onViewportLoad?: Function;
+  onDataLoad?: Function;
+  getFilterValue: Function;
+  filterRange: [number, number];
+  extensions: DataFilterExtension[];
   updateTriggers: {
-    getFilterValue: Record<string, unknown>
-  }
-} & SourceProps
+    getFilterValue: Record<string, unknown>;
+  };
+} & SourceProps;
 
 export type ExecuteSQL = Promise<FeatureCollection | {}[]>;
