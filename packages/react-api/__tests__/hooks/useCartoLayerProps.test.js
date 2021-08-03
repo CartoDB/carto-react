2750da36be42cb014074d132e6488b9d3042ee21
Alberto Asuero <albertoasuero@gmail.com>
2021-08-03 21:13:06 +0200

    Fix tests

--- packages/react-api/__tests__/hooks/useCartoLayerProps.test.js
+++ packages/react-api/__tests__/hooks/useCartoLayerProps.test.js
@@ -121,7 +121,7 @@ describe('useCartoLayerProps', () => {
 
   describe('should has correct filter configurations', () => {
     test('uniqueIdProperty should be undefined', () => {
-      const { result } = renderHook(() => useCartoLayerProps());
+      const { result } = renderHook(() => useCartoLayerProps({}));
 
       expect(result.current.uniqueIdProperty).toBeUndefined();
     });
@@ -166,32 +166,32 @@ describe('useCartoLayerProps', () => {
     });
 
     test('getFilterValue should be a function', () => {
-      const { result } = renderHook(() => useCartoLayerProps());
+      const { result } = renderHook(() => useCartoLayerProps({}));
 
       expect(result.current.getFilterValue).toBeInstanceOf(Function);
     });
 
     test('filter range should be between 1 and 1', () => {
-      const { result } = renderHook(() => useCartoLayerProps());
+      const { result } = renderHook(() => useCartoLayerProps({}));
 
       expect(result.current.filterRange).toEqual([1, 1]);
     });
 
     test('extensions should have an unique instance of DataFilterExtension', () => {
-      const { result } = renderHook(() => useCartoLayerProps());
+      const { result } = renderHook(() => useCartoLayerProps({}));
 
       expect(result.current.extensions.length).toBe(1);
       expect(result.current.extensions[0]).toBeInstanceOf(DataFilterExtension);
     });
 
     test('filter size should be 1', () => {
-      const { result } = renderHook(() => useCartoLayerProps());
+      const { result } = renderHook(() => useCartoLayerProps({}));
 
       expect(result.current.extensions[0].opts.filterSize).toEqual(1);
     });
 
     test('getFilterValue trigger should be present', () => {
-      const { result } = renderHook(() => useCartoLayerProps());
+      const { result } = renderHook(() => useCartoLayerProps({}));
 
       expect(result.current.updateTriggers).toHaveProperty('getFilterValue');
     });
