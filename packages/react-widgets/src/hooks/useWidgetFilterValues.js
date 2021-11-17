a86ad9351e9c64d05f0b13f94d8165c9727c4808
zbigg <zzagorski@spotbeans.com>
2021-11-17 13:47:06 +0100

    useWidgetFilterValues cleanup

--- packages/react-widgets/src/hooks/useWidgetFilterValues.js
+++ packages/react-widgets/src/hooks/useWidgetFilterValues.js
@@ -4,22 +4,23 @@ import useCustomCompareMemo from './useCustomCompareMemo';
 import { dequal as deepEqual } from 'dequal';
 
 /**
- * Obtain current widgets's filter values.
+ * Obtain widget's filter values.
  *
  * @param  {object} props
  * @param  {string} props.dataSource - ID of the source to get the filters from.
- * @param  {string} props.id - ID of the widget that apply the filter you want to exclude.
+ * @param  {string} props.id - ID of the widget that applied the filter.
  * @param  {string} props.column - name of column of this widget.
  * @param  {string} props.type - type of filter
  */
 export function useWidgetFilterValues({ dataSource, id, column, type }) {
   const columnFilters = useSelector((state) => {
-    // TODO: id ?
     const dataSourceState = selectSourceById(state, dataSource);
-    const filterTypes = dataSourceState?.filters ? dataSourceState?.filters[column] : {};
 
-    console.log('useWidgetOwnFilters', dataSource, id, filterTypes);
-    return filterTypes ? filterTypes[type]?.values : null;
+    const filter = dataSourceState?.filters?.[column]?.[type];
+    if (!filter || filter.owner !== id) {
+      return null;
+    }
+    return filter.values;
   });
 
   return useCustomCompareMemo(columnFilters, deepEqual);
