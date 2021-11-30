e83fd3b8523a5246d1c1da24f62f35ef5b4877a2
Sergio Clebal <sclebal@cartodb.com>
2021-11-30 09:05:24 +0100

    Add test for formatterUtils

---
+++ packages/react-ui/__tests__/widgets/utils/formatterUtils.test.js
@@ -0,0 +1,14 @@
+import { processFormatterRes } from '../../../src/widgets/utils/formatterUtils';
+
+describe('Formatter utils', () => {
+    describe('processFormatterRes', () => {
+        test('if formatterRes is an object', () => {
+          const formatterRes = { prefix: '$', value: 5 };
+          expect(processFormatterRes(formatterRes)).toBe('$5');
+        });
+        test('if formatterRes isn\'t an object', () => {
+            const formatterRes = '$5';
+            expect(processFormatterRes(formatterRes)).toBe('$5');
+        });
+    })
+});
