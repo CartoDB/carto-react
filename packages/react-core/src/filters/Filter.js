4237b60673c809e0761552de9cf77670893affa1
Sergio Clebal <sclebal@cartodb.com>
2021-12-07 16:48:46 +0100

    Implement C4R filtering using binary data (#228)

--- packages/react-core/src/filters/Filter.js
+++ packages/react-core/src/filters/Filter.js
@@ -48,7 +48,7 @@ function passesFilter(columns, filters, feature) {
       const filterFunction = filterFunctions[filter];
 
       if (!filterFunction) {
-        throw new Error(`"${filter}" not implemented`);
+        throw new Error(`"${filter}" filter is not implemented.`);
       }
 
       return filterFunction(columnFilters[filter].values, feature[column]);
@@ -76,4 +76,66 @@ export function applyFilters(features, filters) {
   return Object.keys(filters).length
     ? features.filter(buildFeatureFilter({ filters }))
     : features;
+}
+
+// Binary
+export function buildBinaryFeatureFilter({ filters = {} }) {
+  const columns = Object.keys(filters);
+
+  if (!columns.length) {
+    return () => 1;
+  }
+
+  return (featureIdIdx, binaryData) =>
+    passesFilterUsingBinary(columns, filters, featureIdIdx, binaryData);
+}
+
+function getValueFromNumericProps(featureIdIdx, binaryData, { column }) {
+  return binaryData.numericProps[column]?.value[featureIdIdx];
+}
+
+function getValueFromProperties(featureIdIdx, binaryData, { column }) {
+  const propertyIdx = binaryData.featureIds.value[featureIdIdx];
+  return binaryData.properties[propertyIdx]?.[column];
+}
+
+const GET_VALUE_BY_BINARY_PROP = {
+  properties: getValueFromProperties,
+  numericProps: getValueFromNumericProps
+};
+
+function getBinaryPropertyByFilterValues(filterValues) {
+  return typeof filterValues.flat()[0] === 'string' ? 'properties' : 'numericProps';
+}
+
+function getFeatureValue(featureIdIdx, binaryData, filter) {
+  const { column, values } = filter;
+  const binaryProp = getBinaryPropertyByFilterValues(values);
+  const getFeatureValueFn = GET_VALUE_BY_BINARY_PROP[binaryProp];
+  return getFeatureValueFn(featureIdIdx, binaryData, { column });
+}
+
+function passesFilterUsingBinary(columns, filters, featureIdIdx, binaryData) {
+  return columns.every((column) => {
+    const columnFilters = filters[column];
+
+    return Object.entries(columnFilters).every(([type, { values }]) => {
+      const filterFn = filterFunctions[type];
+      if (!filterFn) {
+        throw new Error(`"${type}" filter is not implemented.`);
+      }
+
+      if (!values) return 0;
+
+      const featureValue = getFeatureValue(featureIdIdx, binaryData, {
+        column,
+        type,
+        values
+      });
+
+      if (featureValue === undefined || featureValue === null) return 0;
+
+      return filterFn(values, featureValue);
+    });
+  });
 }
