5bef18dee234bf78c1a965e1bc2eecd01d4fb354
Javier Aragón <jaragon@cartodb.com>
2021-12-15 16:21:52 +0100

    Update packages/react-api/src/hooks/gpu-filter-utils.js
    Co-authored-by: Víctor Velarde <victor.velarde@gmail.com>

--- packages/react-api/src/hooks/gpu-filter-utils.js
+++ packages/react-api/src/hooks/gpu-filter-utils.js
@@ -46,7 +46,7 @@ function getFilterValueUpdateTriggers(filtersWithoutTimeType, timeFilter) {
   if (Object.keys(timeFilter).length > 0) {
     result[timeFilter.column] = {
       ...result[timeFilter.column],
-      [timeFilter.type]: {}
+      [timeFilter.type]: {} // this allows working with other filters, without an impact on performance
     };
   }
   return JSON.stringify(result);
