9f36391705c807dc6445192dac8cb875abe5012b
Sergio Clebal <sclebal@cartodb.com>
2021-12-16 14:28:23 +0100

    Removed unused lines

--- packages/react-api/__tests__/hooks/useFiltersGeoJson.test.js
+++ packages/react-api/__tests__/hooks/useFiltersGeoJson.test.js
@@ -15,17 +15,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-71.0643, 42.35392]
     },
-    properties: {
-      cartodb_id: 302,
-      storetype: 'Drugstore',
-      address: '146 TREMONT ST',
-      city: 'BOSTON',
-      state: 'MA',
-      zip: '2111',
-      store_id: 439,
-      revenue: 1301427,
-      size_m2: 846
-    }
+    properties: { cartodb_id: 302 }
   },
   {
     type: 'Feature',
@@ -33,17 +23,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-71.57506, 44.49876]
     },
-    properties: {
-      cartodb_id: 444,
-      storetype: 'Supermarket',
-      address: '258  MAIN STREET',
-      city: 'LANCASTER',
-      state: 'NH',
-      zip: '3584',
-      store_id: 2158,
-      revenue: 1404681,
-      size_m2: 767
-    }
+    properties: { cartodb_id: 444 }
   },
   {
     type: 'Feature',
@@ -51,17 +31,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-76.98689, 41.22844]
     },
-    properties: {
-      cartodb_id: 1993,
-      storetype: 'Supermarket',
-      address: '180 MONTGOMERY RD',
-      city: 'SOUTH WILLIAMSPORT',
-      state: 'PA',
-      zip: '17702',
-      store_id: 3260,
-      revenue: 1492546,
-      size_m2: 109
-    }
+    properties: { cartodb_id: 1993 }
   },
   {
     type: 'Feature',
@@ -69,17 +39,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-79.17348, 34.81043]
     },
-    properties: {
-      cartodb_id: 3300,
-      storetype: 'Supermarket',
-      address: '613 E 4TH AVE',
-      city: 'RED SPRINGS',
-      state: 'NC',
-      zip: '28377',
-      store_id: 4224,
-      revenue: 1357003,
-      size_m2: 783
-    }
+    properties: { cartodb_id: 3300 }
   },
   {
     type: 'Feature',
@@ -87,17 +47,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-87.67071, 34.7753]
     },
-    properties: {
-      cartodb_id: 5387,
-      storetype: 'Supermarket',
-      address: '4201 JACKSON HWY',
-      city: 'SHEFFIELD',
-      state: 'AL',
-      zip: '35660',
-      store_id: 5711,
-      revenue: 1523344,
-      size_m2: 376
-    }
+    properties: { cartodb_id: 5387 }
   },
   {
     type: 'Feature',
@@ -105,17 +55,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-86.11238, 42.80297]
     },
-    properties: {
-      cartodb_id: 7615,
-      storetype: 'Supermarket',
-      address: '213 N RIVER AVE',
-      city: 'HOLLAND',
-      state: 'MI',
-      zip: '49424',
-      store_id: 7458,
-      revenue: 1424545,
-      size_m2: 520
-    }
+    properties: { cartodb_id: 7615 }
   },
   {
     type: 'Feature',
@@ -123,17 +63,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-96.66324, 35.75362]
     },
-    properties: {
-      cartodb_id: 10236,
-      storetype: 'Supermarket',
-      address: '801 ADA WEBB DR',
-      city: 'STROUD',
-      state: 'OK',
-      zip: '74079',
-      store_id: 9381,
-      revenue: 1583047,
-      size_m2: 760
-    }
+    properties: { cartodb_id: 10236 }
   },
   {
     type: 'Feature',
@@ -141,17 +71,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-117.20269, 32.89341]
     },
-    properties: {
-      cartodb_id: 12921,
-      storetype: 'Supermarket',
-      address: '9440 SCRANTON RD',
-      city: 'SAN DIEGO',
-      state: 'CA',
-      zip: '92121',
-      store_id: 11064,
-      revenue: 1851285,
-      size_m2: 442
-    }
+    properties: { cartodb_id: 12921 }
   },
   {
     type: 'Feature',
@@ -160,15 +80,7 @@ const FEATURES = [
       coordinates: [-87.01792, 35.63505]
     },
     properties: {
-      cartodb_id: 5566,
-      storetype: 'Supermarket',
-      address: '110 BEAR CREEK PIKE',
-      city: 'COLUMBIA',
-      state: 'TN',
-      zip: '38401-2261',
-      store_id: 5839,
-      revenue: 1510718,
-      size_m2: 650
+      cartodb_id: 5566
     }
   },
   {
@@ -177,17 +89,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-96.68754, 43.52982]
     },
-    properties: {
-      cartodb_id: 7919,
-      storetype: 'Supermarket',
-      address: '3101 E 26TH ST',
-      city: 'SIOUX FALLS',
-      state: 'SD',
-      zip: '57103-4033',
-      store_id: 7686,
-      revenue: 1831190,
-      size_m2: 258
-    }
+    properties: { cartodb_id: 7919 }
   },
   {
     type: 'Feature',
@@ -195,17 +97,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-116.21464, 43.59429]
     },
-    properties: {
-      cartodb_id: 11713,
-      storetype: 'Supermarket',
-      address: '1185 VISTA AVE',
-      city: 'BOISE',
-      state: 'ID',
-      zip: '83705-2430',
-      store_id: 10298,
-      revenue: 1588175,
-      size_m2: 886
-    }
+    properties: { cartodb_id: 11713 }
   },
   {
     type: 'Feature',
@@ -213,17 +105,7 @@ const FEATURES = [
       type: 'Point',
       coordinates: [-92.99341, 43.6801]
     },
-    properties: {
-      cartodb_id: 7847,
-      storetype: 'Discount Store',
-      address: '1402 14TH ST NW',
-      city: 'AUSTIN',
-      state: 'MN',
-      zip: '55912',
-      store_id: 967,
-      revenue: 1548809,
-      size_m2: 673
-    }
+    properties: { cartodb_id: 7847 }
   }
 ].map((feature, idx) => ({
   ...feature,
