4288de9f6760f74af20940771fe112c7ec9a3b5a
Sergio Clebal <sclebal@cartodb.com>
2021-12-17 12:39:28 +0100

    Add spatial filter test in dataFilterExtensionUtil tests

--- packages/react-api/__tests__/hooks/dataFilterExtensionUtil.test.js
+++ packages/react-api/__tests__/hooks/dataFilterExtensionUtil.test.js
@@ -8,7 +8,7 @@ describe('dataFilterExtensionUtil', () => {
     expect(MAX_GPU_FILTERS).toEqual(4);
   });
 
-  test('correct filter values without time filter', () => {
+  test('correct filter using C4R filters', () => {
     const filters = {
       storetype: {
         in: {
@@ -51,7 +51,7 @@ describe('dataFilterExtensionUtil', () => {
     expect(getFilterValue(featureNotFilter)).toEqual([0, 0, 0, 0]);
   });
 
-  test.only('correct values with time filter', () => {
+  test('correct filter using also time filter', () => {
     const filters = {
       storetype: {
         in: {
@@ -91,5 +91,71 @@ describe('dataFilterExtensionUtil', () => {
     filters.dateTime.time = {};
     expect(updateTriggers.getFilterValue).toEqual(JSON.stringify(filters));
     expect(getFilterValue(feature)).toEqual([1, feature.properties.dateTime, 0, 0]);
+  });
+
+  test('correct filter using also spatial filter', () => {
+    const filteringGeometry = {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [-70.9643, 41.95392],
+          [-71.0643, 42.35392],
+          [-70.9643, 41.95392]
+        ]
+      ]
+    };
+
+    const filters = {
+      storetype: {
+        in: {
+          values: ['Supermarket'],
+          owner: 'revenueByStoreType'
+        }
+      },
+      dateTime: {
+        time: {
+          values: [[473385600000, 504921600000]],
+          owner: 'storesByRevenue'
+        }
+      }
+    };
+
+    const featurePassesFilter = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-71.0643, 42.35392]
+      },
+      properties: {
+        storetype: 'Supermarket',
+        dateTime: 473385600001
+      }
+    };
+    const featureDoesntPassFilter = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-71.57506, 44.49876]
+      },
+      properties: {
+        storetype: 'Supermarket',
+        dateTime: 0
+      }
+    };
+
+    const { getFilterValue } = getDataFilterExtensionProps(filters, filteringGeometry);
+
+    expect(getFilterValue(featurePassesFilter)).toEqual([
+      1,
+      featurePassesFilter.properties.dateTime,
+      0,
+      0
+    ]);
+    expect(getFilterValue(featureDoesntPassFilter)).toEqual([
+      0,
+      featureDoesntPassFilter.properties.dateTime,
+      0,
+      0
+    ]);
   });
 });
