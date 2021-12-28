e6d942e782362f9b9ec5f67bc45ae31ce98ed589
bbecquet <benjamin.becquet@gmail.com>
2021-12-28 16:04:55 +0100

    Add test for TableWidgetUI

--- packages/react-ui/storybook/stories/widgetsUI/TableWidgetUI.stories.js
+++ packages/react-ui/storybook/stories/widgetsUI/TableWidgetUI.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TableWidgetUI from '../../../src/widgets/TableWidgetUI/TableWidgetUI';
 import { Box, Typography } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { columns, rows } from '../../../src/widgets/TableWidgetUI/mockData';
 
 const options = {
   title: 'Widgets UI/TableWidgetUI',
@@ -10,154 +11,30 @@ const options = {
 };
 export default options;
 
-const COLUMNS = [
-  { field: 'address', headerName: 'Address', sort: true, width: 250 },
-  { field: 'city', headerName: 'City', sort: true },
-  { field: 'state', headerName: 'State / Province', sort: true },
-  { field: 'zip', headerName: 'Post Code', sort: false },
-  { field: 'country', headerName: 'Country', sort: false },
-  { field: 'geocode', headerName: 'Geocode', align: 'right' }
-];
-
-const ROWS = [
-  {
-    id: 1,
-    address: 'Calle Ebro',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 2,
-    address: 'Calle Luis Fuentes Bejarano',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41020',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 3,
-    address: 'Paseo Acacias',
-    city: 'Madrid',
-    state: 'Madrid',
-    zip: '28013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 4,
-    address: 'Av. Luis Fuentes Becerril',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 5,
-    address: 'Calle Luna',
-    city: 'Madrid',
-    state: 'Madrid',
-    zip: '28013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 6,
-    address: 'Av. Luis Montoto',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 7,
-    address: 'Av. Eduardo Dato',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 8,
-    address: 'Calle Gran Via',
-    city: 'Madrid',
-    state: 'Madrid',
-    zip: '28013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 9,
-    address: 'Calle Arroyo',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 10,
-    address: 'Calle Goya',
-    city: 'Madrid',
-    state: 'Madrid',
-    zip: '28013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 11,
-    address: 'Calle Tetuan',
-    city: 'Sevilla',
-    state: 'Sevilla',
-    zip: '41013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  },
-  {
-    id: 12,
-    address: 'Calle Tetuan',
-    city: 'Madrid',
-    state: 'Madrid',
-    zip: '28013',
-    country: 'Spain',
-    geocode: '37.35559, -5.98317'
-  }
-];
-
 const Template = (args) => {
   return <TableWidgetUI {...args} />;
 };
 
-const DefaultProps = {
-  columns: COLUMNS,
-  rows: ROWS
-};
+const DefaultProps = { columns, rows };
 
 export const Playground = Template.bind({});
-Playground.args = { ...DefaultProps, rows: ROWS.slice(0, 5) };
+Playground.args = { ...DefaultProps, rows: rows.slice(0, 5) };
 
 export const Pagination = Template.bind({});
 Pagination.args = {
   ...DefaultProps,
   rowsPerPage: 6,
-  rows: ROWS.slice(0, 6),
+  rows: rows.slice(0, 6),
   page: 0,
   pagination: true,
   rowsPerPageOptions: [4, 6, 8],
-  totalCount: ROWS.length
+  totalCount: rows.length
 };
 
 export const RowClick = Template.bind({});
 RowClick.args = {
   ...DefaultProps,
-  rows: ROWS.slice(0, 5),
-  clickable: true,
+  rows: rows.slice(0, 5),
   onRowClick: (row) => alert(row.address)
 };
 
@@ -177,12 +54,12 @@ function CustomColumnComponent(value) {
 export const CustomComponent = Template.bind({});
 CustomComponent.args = {
   ...DefaultProps,
-  columns: COLUMNS.map((column) => {
+  columns: columns.map((column) => {
     if (column.field === 'geocode') {
       return { ...column, component: CustomColumnComponent };
     } else {
       return column;
     }
   }),
-  rows: ROWS.slice(0, 5)
+  rows: rows.slice(0, 5)
 };
