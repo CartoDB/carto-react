bad33a84e91374f2d198a1cc1186e96618919452
Edgar Bermejo <ebermejo@cartodb.com>
2022-01-21 10:55:08 +0100

    New InputFile component (#276)

---
+++ packages/react-ui/src/assets/UploadIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+export default function RectangleIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path
+        transform='translate(0, 2.000000)'
+        d='M7.5,13.3333333 L12.5,13.3333333 L12.5,8.33333333 L15.8333333,8.33333333 L10,2.5 L4.16666667,8.33333333 L7.5,8.33333333 L7.5,13.3333333 Z M4.16666667,15 L15.8333333,15 L15.8333333,16.6666667 L4.16666667,16.6666667 L4.16666667,15 Z'
+        id='path-1'
+      ></path>
+    </SvgIcon>
+  );
+}
---
+++ packages/react-ui/src/custom-components/InputFile.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Box, Button, makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  inputFile: {
+    flex: 1,
+    width: '100%'
+  },
+  inputWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flex: 1,
+    height: theme.spacing(4.5),
+    marginRight: theme.spacing(1.5),
+    padding: theme.spacing(0, 1),
+    border: `2px solid ${theme.palette.action.disabled}`,
+    borderRadius: theme.spacing(0.5),
+    transition: theme.transitions.create(['border'], {
+      duration: theme.transitions.duration.short,
+      easing: theme.transitions.easing.easeInOut
+    })
+  },
+  inputWrapperOver: {
+    border: `2px solid ${theme.palette.primary.light}`
+  },
+  placeholder: {
+    opacity: 0.42,
+    maxWidth: 'calc(100% - 110px)'
+  },
+  placeholderFiles: {
+    opacity: 1
+  }
+}));
+
+function InputFile(props) {
+  const classes = useStyles();
+  const uploadInputRef = useRef(null);
+  const [filesText, setFilesText] = useState('');
+  const [over, setOver] = useState(false);
+
+  useEffect(() => {
+    if (props.files.length === 0) {
+      setFilesText('');
+    } else if (props.files.length === 1) {
+      setFilesText(props.files[0].name);
+    } else {
+      setFilesText(`${props.files.length} files selected`);
+    }
+  }, [props.files]);
+
+  const handleBrowse = () => {
+    uploadInputRef.current?.click();
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setOver(true);
+  };
+
+  const handleDragLeave = (event) => {
+    event.preventDefault();
+    setOver(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    const items = event.dataTransfer.items;
+    const newFiles = getAllFiles(items);
+    props.onChange(newFiles);
+  };
+
+  const getAllFiles = (items) => {
+    const newFiles = [];
+    for (let item of items) {
+      if (item.kind === 'file') {
+        const file = item.getAsFile();
+        newFiles.push(file);
+      }
+    }
+
+    return newFiles;
+  };
+
+  const handleFiles = (event) => {
+    const newFiles = Array.from(event.target.files);
+    props.onChange(newFiles);
+  };
+
+  return (
+    <Box
+      className={`${classes.inputWrapper} ${over ? classes.inputWrapperOver : ''}`}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
+      <Typography
+        variant='body2'
+        color='textPrimary'
+        noWrap
+        className={`${classes.placeholder} ${filesText ? classes.placeholderFiles : ''}`}
+      >
+        {filesText || props.placeholder}
+      </Typography>
+      <Button size='small' variant='text' color='primary' onClick={handleBrowse}>
+        {props.buttonText}
+      </Button>
+      <input
+        ref={uploadInputRef}
+        style={{ display: 'none' }}
+        type='file'
+        accept={props.accept}
+        multiple={props.multiple}
+        onChange={handleFiles}
+      />
+    </Box>
+  );
+}
+
+InputFile.defaultProps = {
+  placeholder: 'Drag and drop your file or click to browse',
+  buttonText: 'Browse',
+  accept: 'application/JSON',
+  multiple: false,
+  files: [],
+  onChange: (files) => files
+};
+
+InputFile.propTypes = {
+  placeholder: PropTypes.string,
+  buttonText: PropTypes.string,
+  accept: PropTypes.string,
+  multiple: PropTypes.bool,
+  files: PropTypes.array,
+  onChange: PropTypes.func.isRequired
+};
+
+export default InputFile;
