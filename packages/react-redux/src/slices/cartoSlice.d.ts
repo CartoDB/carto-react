bc65dd920f147da1fa1486862e298a5c909d0601
Iker Garcia <ikergarciadev@gmail.com>
2022-01-25 17:37:41 +0100

    fix: remove auto file format

--- packages/react-redux/src/slices/cartoSlice.d.ts
+++ packages/react-redux/src/slices/cartoSlice.d.ts
@@ -1,6 +1,6 @@
 import { Credentials } from '@carto/react-api/';
 import { SourceProps } from '@carto/react-api/types';
-import { _FilterTypes } from '@carto/react-core/types'
+import { _FilterTypes } from '@carto/react-core/types';
 import { CartoBasemapsNames, GMapsBasemapsNames } from '@carto/react-basemaps/';
 import { InitialCartoState, CartoState, ViewState } from '../types';
 import { AnyAction, Reducer } from 'redux';
@@ -63,10 +63,12 @@ declare enum CartoActions {
   SET_FEATURES_READY = 'carto/setFeaturesReady',
   SET_CREDENTIALS = 'carto/setCredentials',
   SET_DRAWING_TOOL_MODE = 'carto/setDrawingToolMode',
-  SET_DRAWING_TOOL_ENABLED = 'carto/setDrawingToolEnabled',
+  SET_DRAWING_TOOL_ENABLED = 'carto/setDrawingToolEnabled'
 }
 
-export function createCartoSlice(initialState: InitialCartoState): Reducer<CartoState, AnyAction>;
+export function createCartoSlice(
+  initialState: InitialCartoState
+): Reducer<CartoState, AnyAction>;
 
 export function addSource(source: Source): {
   type: CartoActions.ADD_SOURCE;
