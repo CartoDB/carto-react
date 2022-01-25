81d8da57a731b44ede7d07f65116626c32da649e
Iker Garcia <ikergarciadev@gmail.com>
2022-01-25 17:36:11 +0100

    fix: remove auto file format

--- packages/react-core/src/types.d.ts
+++ packages/react-core/src/types.d.ts
@@ -1,33 +1,33 @@
 import { AggregationTypes } from './operations/aggregation/AggregationTypes';
 import { Geometry } from 'geojson';
-import { FilterTypes } from './filters/FilterQueryBuilder';
+import {
+  FilterTypes
+} from './filters/FilterQueryBuilder';
 
 export type AggregationFunctions = {
-  [AggregationTypes.COUNT]: Function;
-  [AggregationTypes.MIN]: Function;
-  [AggregationTypes.MAX]: Function;
-  [AggregationTypes.SUM]: Function;
-  [AggregationTypes.AVG]: Function;
-};
+  [AggregationTypes.COUNT]: Function,
+  [AggregationTypes.MIN]: Function,
+  [AggregationTypes.MAX]: Function,
+  [AggregationTypes.SUM]: Function,
+  [AggregationTypes.AVG]: Function
+}
 
-export type GroupByFeature =
-  | {
-      name: string;
-      value: number;
-    }[]
-  | [];
+export type GroupByFeature = {
+  name: string,
+  value: number
+}[] | [];
 
 export type HistogramFeature = number[] | [];
 
 export type Viewport = [number, number, number, number];
 
 export type TileFeatures = {
-  tiles: any; // TODO: add proper deck.gl type
-  viewport: Viewport;
-  geometry?: Geometry;
-  uniqueIdProperty?: string;
-};
+  tiles: any, // TODO: add proper deck.gl type
+  viewport: Viewport,
+  geometry?: Geometry,
+  uniqueIdProperty?: string
+}
 
-export type TileFeaturesResponse = Record<string, unknown>[] | [];
+export type TileFeaturesResponse = Record<string, unknown>[] | []
 
 export type _FilterTypes = typeof FilterTypes;
