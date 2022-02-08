f28b1cdedafc647b3b5a9298dd409670273874ca
Javier Aragón <jaragon@cartodb.com>
2022-02-08 11:00:25 +0100

    Support for tiles in WGS84 (#312)

--- packages/react-core/src/types.d.ts
+++ packages/react-core/src/types.d.ts
@@ -1,3 +1,4 @@
+import { TILE_FORMATS } from '@deck.gl/carto'
 import { AggregationTypes } from './operations/aggregation/AggregationTypes';
 import { Geometry } from 'geojson';
 import {
@@ -25,7 +26,8 @@ export type TileFeatures = {
   tiles: any, // TODO: add proper deck.gl type
   viewport: Viewport,
   geometry?: Geometry,
-  uniqueIdProperty?: string
+  uniqueIdProperty?: string,
+  tileFormat: TILE_FORMATS
 }
 
 export type TileFeaturesResponse = Record<string, unknown>[] | []
