b0c9d282cc60cc2cd9fcc1a5d949d153561a051b
Sergio Clebal <sclebal@cartodb.com>
2022-02-17 13:57:26 +0100

    Extends ReactEcharts class

--- packages/react-ui/src/custom-components/echarts-for-react/helper/is-function.js
+++
@@ -1,7 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isFunction = void 0;
-function isFunction(v) {
-    return typeof v === 'function';
-}
-exports.isFunction = isFunction;
-//# sourceMappingURL=is-function.js.map
