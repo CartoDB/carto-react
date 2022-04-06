f152a42162298853dc6b694e395bb197a102e6e7
Sergio Clebal <sclebal@cartodb.com>
2022-04-06 09:56:55 +0200

    Improvements in model checking

--- packages/react-widgets/src/models/utils.js
+++ packages/react-widgets/src/models/utils.js
@@ -1,13 +1,19 @@
 import { AggregationTypes, _filtersToSQL } from '@carto/react-core';
-import { MAP_TYPES } from '@deck.gl/carto';
+import { MAP_TYPES, API_VERSIONS } from '@deck.gl/carto';
 
 export function wrapModelCall(props, fromLocal, fromRemote) {
   const { source, global } = props;
 
-  if (source.type === MAP_TYPES.TILESET)
-    throw new Error('Tileset sources are not supported');
-
   if (global) {
+    if (source.type === MAP_TYPES.TILESET)
+      throw new Error('Tileset sources are not supported in global mode.');
+
+    if (source.credentials.apiVersion === API_VERSIONS.V2) {
+      throw new Error(
+        'CARTO 2 cannot be used in global mode. Upgrade to CARTO 3, please.'
+      );
+    }
+
     return fromRemote(props);
   }
 
