007aa88a558d61b61b961f7ce356fff2c3cb03d6
Sergio Clebal <sclebal@cartodb.com>
2022-04-07 15:37:39 +0200

    HistogramWidget global remote mode [sc-219450]

--- packages/react-widgets/src/models/HistogramModel.js
+++ packages/react-widgets/src/models/HistogramModel.js
@@ -1,13 +1,54 @@
+import { executeSQL } from '@carto/react-api';
 import { Methods, executeTask } from '@carto/react-workers';
+import { formatTableNameWithFilters, wrapModelCall } from './utils';
 
-export const getHistogram = async (props) => {
-  const { column, operation, ticks, filters, filtersLogicalOperator, dataSource } = props;
+export function getHistogram(props) {
+  return wrapModelCall(props, fromLocal, fromRemote);
+}
 
-  return executeTask(dataSource, Methods.FEATURES_HISTOGRAM, {
-    filters,
-    filtersLogicalOperator,
+// From local
+function fromLocal(props) {
+  const { source, column, operation, ticks } = props;
+
+  return executeTask(source.id, Methods.FEATURES_HISTOGRAM, {
+    filters: source.filters,
+    filtersLogicalOperator: source.filtersLogicalOperator,
     operation,
     column,
     ticks
   });
-};
+}
+
+// From remote
+async function fromRemote(props) {
+  const { source, ticks, abortController } = props;
+  const { credentials, connection } = source;
+
+  const query = buildSqlQueryToGetHistogram(props);
+
+  const data = await executeSQL({
+    credentials,
+    query,
+    connection,
+    opts: { abortController }
+  });
+
+  const result = Array(ticks.length + 1).fill(0);
+  data.forEach(({ tick, value }) => (result[tick] = value));
+
+  return result;
+}
+
+function buildSqlQueryToGetHistogram(props) {
+  const { column, operation, ticks } = props;
+
+  const caseTicks = ticks.map((t, index) => `WHEN ${column} < ${t} THEN ${index}`);
+  caseTicks.push(`ELSE ${ticks.length}`);
+
+  const selectValueClause = `${operation}(${column}) as value`;
+  const selectTickClause = `CASE ${caseTicks.join(' ')} END as tick`;
+
+  const subQuery = `SELECT ${selectTickClause}, ${column} FROM ${formatTableNameWithFilters(props)}`;
+
+  return `SELECT tick, ${selectValueClause} FROM (${subQuery}) q GROUP BY tick`;
+}
