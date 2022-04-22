860b947497fa55e409489eb6184ba7ab0b0969fc
Sergio Clebal <sclebal@cartodb.com>
2022-04-22 10:30:38 +0200

    Implement CLOSED_OPEN and TIME filters for SQL

--- packages/react-core/src/filters/FilterQueryBuilder.js
+++ packages/react-core/src/filters/FilterQueryBuilder.js
@@ -17,39 +17,68 @@ export const getApplicableFilters = (filters = {}, owner) => {
   return filtersCopy;
 };
 
+// Filters to SQL
+
+const filterFunctions = {
+  [FilterTypes.IN](column, filterValues) {
+    const formattedValues = filterValues.map((v) => (isFinite(v) ? v : `'${v}'`));
+    return `${column} in(${formattedValues})`;
+  },
+  [FilterTypes.BETWEEN](column, filterValues) {
+    const queryFilters = filterValues.map(
+      ([left, right]) =>
+        `${left ? `${column} >= ${left}` : ``} ${left && right ? ' and ' : ''} ${
+          right ? `${column} <= ${right}` : ``
+        }`
+    );
+
+    return joinFilters(queryFilters);
+  },
+  [FilterTypes.TIME](column, filterValues) {
+    const formattedValues = filterValues.map((values) => [
+      `cast('${new Date(values[0]).toISOString()}' as timestamp)`,
+      `cast('${new Date(values[1]).toISOString()}' as timestamp)`
+    ]);
+    const tsColumn = `cast(${column} as timestamp)`;
+    const queryFilters = formattedValues.map(
+      ([left, right]) =>
+        `${left ? `${tsColumn} >= ${left}` : ``} ${left && right ? ' and ' : ''} ${
+          right ? `${tsColumn} <= ${right}` : ``
+        }`
+    );
+
+    return joinFilters(queryFilters);
+  },
+  [FilterTypes.CLOSED_OPEN](column, filterValues) {
+    const queryFilters = filterValues.map(
+      ([left, right]) =>
+        `${left ? `${column} >= ${left}` : ``} ${left && right ? ' and ' : ''} ${
+          right ? `${column} < ${right}` : ``
+        }`
+    );
+
+    return joinFilters(queryFilters);
+  }
+};
+
 export const filtersToSQL = (
   filters = {},
   filtersLogicalOperator = FiltersLogicalOperators.AND
 ) => {
   const result = [];
 
-  Object.entries(filters).forEach(([column, filter]) => {
-    Object.entries(filter).forEach(([operator, params]) => {
-      switch (operator) {
-        case FilterTypes.IN:
-          result.push(
-            `${column} ${operator}(${params.values
-              .map((v) => (isFinite(v) ? v : `'${v}'`))
-              .join(',')})`
-          );
-          break;
-        case FilterTypes.BETWEEN:
-          result.push(
-            `(${params.values
-              .map(
-                ([left, right]) =>
-                  `${left ? `${column} >= ${left}` : ``} ${
-                    left && right ? ' and ' : ''
-                  } ${right ? `${column} < ${right}` : ``}`
-              )
-              .join(') OR (')})`
-          );
-          break;
-        default:
-          throw new Error(`Not valid operator has provided: ${operator}`);
-      }
+  Object.entries(filters).forEach(([column, filters]) => {
+    Object.entries(filters).forEach(([operator, filter]) => {
+      const filterFn = filterFunctions[operator];
+      if (!filterFn) throw new Error(`Filter ${operator} is not defined.`);
+      result.push(filterFn(column, filter.values, filter.params));
     });
   });
 
-  return result.length ? `WHERE (${result.join(`) ${filtersLogicalOperator} (`)})` : '';
+  return result.length ? `WHERE ${joinFilters(result, filtersLogicalOperator)}` : '';
 };
+
+// Aux
+function joinFilters(queryFilters, operator = FiltersLogicalOperators.OR) {
+  return queryFilters.map((queryFilter) => `(${queryFilter})`).join(` ${operator} `);
+}
