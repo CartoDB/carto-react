32f03e4579441726505968f0be4d1e5b6999a564
Sergio Clebal <sclebal@cartodb.com>
2022-04-29 13:13:12 +0200

    HistogramWidget/HistogramWidgetUI refactor (#388)

--- packages/react-widgets/src/models/HistogramModel.js
+++ packages/react-widgets/src/models/HistogramModel.js
@@ -8,11 +8,12 @@ export function getHistogram(props) {
 
 // From local
 function fromLocal(props) {
-  const { source, column, operation, ticks } = props;
+  const { source, column, operation, ticks, bins } = props;
 
   return executeTask(source.id, Methods.FEATURES_HISTOGRAM, {
     filters: source.filters,
     filtersLogicalOperator: source.filtersLogicalOperator,
+    bins,
     operation,
     column,
     ticks
@@ -21,7 +22,7 @@ function fromLocal(props) {
 
 // From remote
 async function fromRemote(props) {
-  const { source, ticks, abortController } = props;
+  const { source, ticks, bins, abortController } = props;
   const { credentials, connection } = source;
 
   const query = buildSqlQueryToGetHistogram(props);
@@ -33,22 +34,48 @@ async function fromRemote(props) {
     opts: { abortController }
   }).then(normalizeObjectKeys);
 
-  const result = Array(ticks.length + 1).fill(0);
+  const nBreaks = ticks.length ? ticks.length + 1 : bins;
+
+  const result = Array(nBreaks).fill(0);
   data.forEach(({ tick, value }) => (result[tick] = value));
 
-  return result;
+  const min = data[0]._min;
+  const max = data[0]._max;
+
+  return {
+    min,
+    max,
+    data: result,
+    ticks: ticks.length
+      ? ticks
+      : Array(nBreaks - 1)
+          .fill(0)
+          .map((_, index) => min + (max - min) * ((index + 1) / nBreaks))
+  };
 }
 
 function buildSqlQueryToGetHistogram(props) {
-  const { column, operation, ticks } = props;
+  const { column, operation, ticks, bins } = props;
 
-  const caseTicks = ticks.map((t, index) => `WHEN ${column} < ${t} THEN ${index}`);
-  caseTicks.push(`ELSE ${ticks.length}`);
+  const breaks = ticks.length ? ticks : Array(bins - 1).fill(-1);
+  const caseTicks = breaks.map(
+    (t, index) =>
+      `WHEN ${column} < ${
+        t === -1
+          ? `(minMax._min + (minMax._max - minMax._min) * (${index + 1} / ${bins}))`
+          : t
+      } THEN ${index}`
+  );
+  caseTicks.push(`ELSE ${breaks.length}`);
 
   const selectValueClause = `${operation}(${column}) as value`;
   const selectTickClause = `CASE ${caseTicks.join(' ')} END as tick`;
 
-  const subQuery = `SELECT ${selectTickClause}, ${column} FROM ${formatTableNameWithFilters(props)}`;
+  const tableName = formatTableNameWithFilters(props);
 
-  return `SELECT tick, ${selectValueClause} FROM (${subQuery}) q GROUP BY tick`;
+  const minMaxQuery = `SELECT MIN(${column}) _min, MAX(${column}) _max FROM ${tableName}`;
+
+  const subQuery = `SELECT ${selectTickClause}, ${column}, minMax.* FROM ${tableName}, (${minMaxQuery}) minMax`;
+
+  return `SELECT tick, ${selectValueClause}, MIN(q._min) _min, MAX(q._max) _max FROM (${subQuery}) q GROUP BY tick`;
 }
