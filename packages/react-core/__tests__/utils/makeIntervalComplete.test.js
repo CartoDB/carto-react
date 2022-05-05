ba6e3bdbecea846d2c9a497f1d1d429675a813d2
bbecquet <benjamin.becquet@gmail.com>
2022-05-05 15:18:21 +0200

    Fix filters with null as upper or lower bound

--- packages/react-core/__tests__/utils/makeIntervalComplete.test.js
+++ packages/react-core/__tests__/utils/makeIntervalComplete.test.js
@@ -6,12 +6,22 @@ describe('make interval complete', () => {
     expect(makeIntervalComplete(DATA)).toEqual([[Number.MIN_SAFE_INTEGER, 1]]);
   });
 
+  test('first value is null', () => {
+    const DATA = [[null, 1]];
+    expect(makeIntervalComplete(DATA)).toEqual([[Number.MIN_SAFE_INTEGER, 1]]);
+  });
+
   test('last value is undefined', () => {
     const DATA = [[1, undefined]];
     expect(makeIntervalComplete(DATA)).toEqual([[1, Number.MAX_SAFE_INTEGER]]);
   });
 
-  test('both values are not undefined', () => {
+  test('last value is null', () => {
+    const DATA = [[1, null]];
+    expect(makeIntervalComplete(DATA)).toEqual([[1, Number.MAX_SAFE_INTEGER]]);
+  });
+
+  test('both values are not undefined or null', () => {
     const DATA = [[1, 1]];
     expect(makeIntervalComplete(DATA)).toEqual([[1, 1]]);
   });
