b447d3dbb2a865df3e4d7e85330d829468f2c51b
Sergio Clebal <sclebal@cartodb.com>
2022-05-12 08:12:27 +0200

    Use static bins in HistogramWidget (#402)

--- packages/react-core/src/operations/histogram.js
+++ packages/react-core/src/operations/histogram.js
@@ -1,34 +1,8 @@
 import { aggregate, aggregationFunctions } from './aggregation';
-import { AggregationTypes } from './constants/AggregationTypes';
 
-export function histogram({
-  data,
-  valuesColumns,
-  joinOperation,
-  bins = 15,
-  ticks: _ticks = [],
-  operation
-}) {
+export function histogram({ data, valuesColumns, joinOperation, ticks, operation }) {
   if (Array.isArray(data) && data.length === 0) {
-    return {};
-  }
-  const ticks = [..._ticks];
-  const useBins = !ticks.length && bins;
-
-  let min = Number.MAX_SAFE_INTEGER;
-  let max = Number.MIN_SAFE_INTEGER;
-
-  if (useBins) {
-    min = aggregationFunctions[AggregationTypes.MIN](data, valuesColumns, joinOperation);
-    max = aggregationFunctions[AggregationTypes.MAX](data, valuesColumns, joinOperation);
-
-    if (!isFinite(min) || !isFinite(max)) {
-      throw new Error('Cannot calculate histogram without valid data');
-    }
-
-    for (let i = 1; i < bins; i += 1) {
-      ticks.push(min + (max - min) * (i / bins));
-    }
+    return [];
   }
 
   const binsContainer = [Number.MIN_SAFE_INTEGER, ...ticks].map((tick, index, arr) => ({
@@ -47,14 +21,6 @@ export function histogram({
       return;
     }
 
-    if (!useBins) {
-      if (featureValue < min) {
-        min = featureValue;
-      } else if (featureValue > max) {
-        max = featureValue;
-      }
-    }
-
     const binContainer = binsContainer.find(
       (bin) => bin.start <= featureValue && bin.end > featureValue
     );
@@ -70,13 +36,8 @@ export function histogram({
 
   if (targetOperation) {
     const transformedBins = binsContainer.map((binContainer) => binContainer.values);
-    return {
-      min,
-      max,
-      ticks,
-      data: transformedBins.map((values) => (values.length ? targetOperation(values) : 0))
-    };
+    return transformedBins.map((values) => (values.length ? targetOperation(values) : 0));
   }
 
-  return {};
+  return [];
 }
