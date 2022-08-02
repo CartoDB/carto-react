2b2047ecdb32aa6488e1cdf1674595bc68deac52
Eduardo <eamadorpaton@gmail.com>
2022-08-02 12:33:27 +0200

    React-api - model test include case for query with queryParameters

--- packages/react-api/__tests__/api/model.test.js
+++ packages/react-api/__tests__/api/model.test.js
@@ -1,5 +1,5 @@
 import { executeModel } from '../../src/api/model';
-import { QUERY_SOURCE, TABLE_SOURCE, TILESET_SOURCE } from '../dataMocks';
+import { QUERY_PARAMS_SOURCE, QUERY_SOURCE, TABLE_SOURCE, TILESET_SOURCE } from '../dataMocks';
 
 const mockedMakeCall = jest.fn();
 
@@ -38,7 +38,6 @@ describe('model', () => {
     test('works correctly when the source is very large', () => {
       const longQuerySource = {
         ...QUERY_SOURCE,
-        queryParameters: '',
         data: 'a'.repeat(2048)
       };
 
@@ -58,6 +57,17 @@ describe('model', () => {
           })
         },
         url: 'https://gcp-us-east1.api.carto.com/v3/sql/carto-ps-bq-developers/model/formula'
+      });
+    });
+
+
+    test('works correctly when the source has queryParameters', () => {
+      executeModel({ model: 'formula', source: QUERY_PARAMS_SOURCE, params: DEFAULT_PARAMS });
+
+      expect(mockedMakeCall).toHaveBeenCalledWith({
+        credentials: TABLE_SOURCE.credentials,
+        opts: { method: 'GET' },
+        url: 'https://gcp-us-east1.api.carto.com/v3/sql/carto-ps-bq-developers/model/formula?type=query&source=SELECT+*+FROM+%60cartobq.public_account.seattle_collisions%60+WHERE+time_column+%3E+%40start+AND+time_column+%3C+%40end&params=%7B%22column%22%3A%22__test__%22%2C%22operation%22%3A%22avg%22%7D&queryParameters=%7B%22start%22%3A%222019-01-01%22%2C%22end%22%3A%222019-01-02%22%7D&filters=%7B%7D&filtersLogicalOperator=AND'
       });
     });
 
--- packages/react-api/__tests__/dataMocks.js
+++ packages/react-api/__tests__/dataMocks.js
@@ -24,6 +24,20 @@ export const QUERY_SOURCE = {
   }
 };
 
+export const QUERY_PARAMS_SOURCE = {
+  type: 'query',
+  data: 'SELECT * FROM `cartobq.public_account.seattle_collisions` WHERE time_column > @start AND time_column < @end',
+  connection: 'carto-ps-bq-developers',
+  filters: {},
+  queryParameters: {'start': '2019-01-01', 'end': '2019-01-02'},
+  filtersLogicalOperator: 'AND',
+  credentials: {
+    accessToken: '__test_api_key__',
+    apiBaseUrl: 'https://gcp-us-east1.api.carto.com',
+    apiVersion: 'v3'
+  }
+};
+
 export const TILESET_SOURCE = {
   type: 'tileset',
   data: 'cartobq.public_account.pois',
