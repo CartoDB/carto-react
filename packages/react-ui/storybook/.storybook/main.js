409970bf8340e4d39b2378d5b8ddeb730f7e3397
vmilan <vmilan@cartodb.com>
2022-10-05 17:04:51 +0200

    update storybook to v7

--- packages/react-ui/storybook/.storybook/main.js
+++ packages/react-ui/storybook/.storybook/main.js
@@ -1,4 +1,18 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5'
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: { fastRefresh: true }
+  },
+  // It solves the fully specified breaking change: https://github.com/storybookjs/storybook/issues/15335
+  async webpackFinal(config) {
+    config.module.rules.push({
+      resolve: { fullySpecified: false }
+    });
+    return config;
+  }
 };
