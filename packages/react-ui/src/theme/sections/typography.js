5ff5a99c3e9b2e69535c0e6b4087d55ac675bd62
vmilan <vmilan@cartodb.com>
2022-10-18 18:17:06 +0200

    new variants added

--- packages/react-ui/src/theme/sections/typography.js
+++ packages/react-ui/src/theme/sections/typography.js
@@ -1,6 +1,6 @@
 import { getPixelToRem } from '../themeUtils';
 
-export const themeTypography = {
+const baseTypography = {
   htmlFontSize: 16,
   fontFamily: 'Inter, sans-serif',
   fontSize: 16,
@@ -14,14 +14,14 @@ export const themeTypography = {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: getPixelToRem(96),
-    lineHeight: 1.084,
+    lineHeight: 1.167,
     letterSpacing: '-1.5px'
   },
   h2: {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
-    fontSize: getPixelToRem(72),
-    lineHeight: 1.125,
+    fontSize: getPixelToRem(60),
+    lineHeight: 1.2,
     letterSpacing: '-0.5px'
   },
   h3: {
@@ -35,21 +35,21 @@ export const themeTypography = {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: getPixelToRem(32),
-    lineHeight: 1.177,
+    lineHeight: 1.25,
     letterSpacing: '0.15px'
   },
   h5: {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: getPixelToRem(24),
-    lineHeight: 1.334,
+    lineHeight: 1.167,
     letterSpacing: '0.15px'
   },
   h6: {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: getPixelToRem(18),
-    lineHeight: 1.2,
+    lineHeight: 1.333,
     letterSpacing: '0.15px'
   },
   subtitle1: {
@@ -63,7 +63,7 @@ export const themeTypography = {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 600,
     fontSize: getPixelToRem(13),
-    lineHeight: 1.715,
+    lineHeight: 1.538,
     letterSpacing: '0.1px'
   },
   body1: {
@@ -77,14 +77,14 @@ export const themeTypography = {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 400,
     fontSize: getPixelToRem(13),
-    lineHeight: 1.429,
+    lineHeight: 1.538,
     letterSpacing: '0.25px'
   },
   button: {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 500,
     fontSize: getPixelToRem(13),
-    lineHeight: 1.715,
+    lineHeight: 1.538,
     letterSpacing: '0.25px',
     textTransform: 'inherit'
   },
@@ -92,24 +92,138 @@ export const themeTypography = {
     fontFamily: 'Inter, sans-serif',
     fontWeight: 400,
     fontSize: getPixelToRem(11),
-    lineHeight: 1.334,
+    lineHeight: 1.454,
     letterSpacing: '0.2px'
   },
   overline: {
     fontFamily: '"Overpass Mono", monospace',
     fontWeight: 600,
     fontSize: getPixelToRem(10),
-    lineHeight: 1.6,
+    lineHeight: 1.2,
     letterSpacing: '2px',
     textTransform: 'uppercase'
+  }
+};
+
+const customTypography = {
+  body1Italic: {
+    ...baseTypography.body1,
+    fontStyle: 'italic'
   },
-  // Custom fonts
+  body1Strong: {
+    ...baseTypography.body1,
+    fontWeight: 600
+  },
+  body1StrongItalic: {
+    ...baseTypography.body1,
+    fontWeight: 600,
+    fontStyle: 'italic'
+  },
+  body2Italic: {
+    ...baseTypography.body2,
+    fontStyle: 'italic'
+  },
+  body2Strong: {
+    ...baseTypography.body2,
+    fontWeight: 600
+  },
+  body2StrongItalic: {
+    ...baseTypography.body2,
+    fontWeight: 600,
+    fontStyle: 'italic'
+  },
+  buttonLarge: {
+    fontFamily: 'Inter, sans-serif',
+    fontWeight: 500,
+    fontSize: getPixelToRem(16),
+    lineHeight: 1.5,
+    letterSpacing: '0.25px'
+  },
+  buttonSmall: {
+    fontFamily: 'Inter, sans-serif',
+    fontWeight: 500,
+    fontSize: getPixelToRem(11),
+    lineHeight: 1.454,
+    letterSpacing: '0.4px'
+  },
+  captionItalic: {
+    ...baseTypography.caption,
+    fontStyle: 'italic'
+  },
+  captionStrong: {
+    ...baseTypography.caption,
+    fontWeight: 500
+  },
+  captionStrongItalic: {
+    ...baseTypography.caption,
+    fontWeight: 500,
+    fontStyle: 'italic'
+  },
+  overlineDelicate: {
+    ...baseTypography.overline,
+    fontWeight: 400,
+    letterSpacing: '1.2px'
+  },
+  codeLarge: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 400,
+    fontSize: getPixelToRem(16),
+    lineHeight: 1.5,
+    letterSpacing: 0
+  },
+  codeLargeStrong: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 600,
+    fontSize: getPixelToRem(16),
+    lineHeight: 1.5,
+    letterSpacing: 0
+  },
+  codeMedium: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 400,
+    fontSize: getPixelToRem(14),
+    lineHeight: 1.428,
+    letterSpacing: 0
+  },
+  codeMediumStrong: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 600,
+    fontSize: getPixelToRem(14),
+    lineHeight: 1.428,
+    letterSpacing: 0
+  },
+  codeSmall: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 400,
+    fontSize: getPixelToRem(12),
+    lineHeight: 1.333,
+    letterSpacing: 0
+  },
+  codeSmallStrong: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 600,
+    fontSize: getPixelToRem(12),
+    lineHeight: 1.333,
+    letterSpacing: 0
+  },
+  codeSmallChip: {
+    fontFamily: '"Overpass Mono", monospace',
+    fontWeight: 600,
+    fontSize: getPixelToRem(12),
+    lineHeight: 1.25,
+    letterSpacing: 0
+  },
   charts: {
-    // TODO: design is going to create a new variant (more reusable) to replace this
+    // TODO: design team is going to create a new variant (more reusable) to replace this one
     fontFamily: 'Inter, sans-serif',
     fontWeight: 400,
     fontSize: getPixelToRem(10),
-    lineHeight: 1.6,
-    letterSpacing: '0.150em'
+    lineHeight: 1.2,
+    letterSpacing: '2px'
   }
+};
+
+export const themeTypography = {
+  ...baseTypography,
+  ...customTypography
 };
--- packages/react-ui/storybook/stories/common/Typography.stories.js
+++ packages/react-ui/storybook/stories/common/Typography.stories.js
@@ -21,7 +21,27 @@ const options = {
           'body2',
           'button',
           'caption',
-          'overline'
+          'overline',
+          'body1Italic',
+          'body1Strong',
+          'body1StrongItalic',
+          'body2Italic',
+          'body2Strong',
+          'body2StrongItalic',
+          'buttonLarge',
+          'buttonSmall',
+          'captionItalic',
+          'captionStrong',
+          'captionStrongItalic',
+          'overlineDelicate',
+          'codeLarge',
+          'codeLargeStrong',
+          'codeMediun',
+          'codeMediunStrong',
+          'codeSmall',
+          'codeSmallStrong',
+          'codeSmallChip',
+          'charts'
         ]
       }
     }
@@ -37,58 +57,124 @@ const options = {
 export default options;
 
 const Template = (args) => <Typography {...args}>{args.text}</Typography>;
-const disabledControlsArgTypes = {
-  variant: { table: { disable: true } }
-};
 
 export const H1 = Template.bind({});
 H1.args = { variant: 'h1', text: 'H1 Headline' };
-H1.argTypes = disabledControlsArgTypes;
 
 export const H2 = Template.bind({});
 H2.args = { variant: 'h2', text: 'H2 Headline' };
-H2.argTypes = disabledControlsArgTypes;
 
 export const H3 = Template.bind({});
 H3.args = { variant: 'h3', text: 'H3 Headline' };
-H3.argTypes = disabledControlsArgTypes;
 
 export const H4 = Template.bind({});
 H4.args = { variant: 'h4', text: 'H4 Headline' };
-H4.argTypes = disabledControlsArgTypes;
 
 export const H5 = Template.bind({});
 H5.args = { variant: 'h5', text: 'H5 Headline' };
-H5.argTypes = disabledControlsArgTypes;
 
 export const H6 = Template.bind({});
 H6.args = { variant: 'h6', text: 'H6 Headline' };
-H6.argTypes = disabledControlsArgTypes;
 
 export const Subtitle1 = Template.bind({});
 Subtitle1.args = { variant: 'subtitle1', text: 'Subtitle 1' };
-Subtitle1.argTypes = disabledControlsArgTypes;
 
 export const Subtitle2 = Template.bind({});
 Subtitle2.args = { variant: 'subtitle2', text: 'Subtitle 2' };
-Subtitle2.argTypes = disabledControlsArgTypes;
 
 export const Body1 = Template.bind({});
 Body1.args = { variant: 'body1', text: 'Body 1' };
-Body1.argTypes = disabledControlsArgTypes;
 
 export const Body2 = Template.bind({});
 Body2.args = { variant: 'body2', text: 'Body 2' };
-Body2.argTypes = disabledControlsArgTypes;
 
 export const Button = Template.bind({});
 Button.args = { variant: 'button', text: 'Button' };
-Button.argTypes = disabledControlsArgTypes;
 
 export const Caption = Template.bind({});
 Caption.args = { variant: 'caption', text: 'Caption' };
-Caption.argTypes = disabledControlsArgTypes;
 
 export const Overline = Template.bind({});
 Overline.args = { variant: 'overline', text: 'Overline' };
-Overline.argTypes = disabledControlsArgTypes;
+
+// Custom variants
+export const Body1Italic = Template.bind({});
+Body1Italic.args = { variant: 'body1Italic', text: 'Body 1 Italic' };
+
+export const Body1Strong = Template.bind({});
+Body1Strong.args = { variant: 'boy1Strong', text: 'Body 1 Strong' };
+
+export const Body1StrongItalic = Template.bind({});
+Body1StrongItalic.args = { variant: 'body1StrongItalic', text: 'Body 1 Strong Italic' };
+
+export const Body2Italic = Template.bind({});
+Body2Italic.args = { variant: 'body2Italic', text: 'Body 2 Italic' };
+
+export const Body2Strong = Template.bind({});
+Body2Strong.args = { variant: 'body2Strong', text: 'Body 2 Strong' };
+
+export const Body2StrongItalic = Template.bind({});
+Body2StrongItalic.args = { variant: 'body2StrongItalic', text: 'Body 2 Strong Italic' };
+
+export const ButtonLarge = Template.bind({});
+ButtonLarge.args = { variant: 'buttonLarge', text: 'Button Large' };
+
+export const ButtonSmall = Template.bind({});
+ButtonSmall.args = { variant: 'buttonSmall', text: 'Button Small' };
+
+export const CaptionItalic = Template.bind({});
+CaptionItalic.args = { variant: 'captionItalic', text: 'Caption Italic' };
+
+export const CaptionStrong = Template.bind({});
+CaptionStrong.args = { variant: 'captionStrong', text: 'Caption Strong' };
+
+export const CaptionStrongItalic = Template.bind({});
+CaptionStrongItalic.args = {
+  variant: 'captionStrongItalic',
+  text: 'Caption Strong Italic'
+};
+
+export const CodeLarge = Template.bind({});
+CodeLarge.args = {
+  variant: 'codeLarge',
+  text: 'Code Large'
+};
+
+export const CodeLargeStrong = Template.bind({});
+CodeLargeStrong.args = {
+  variant: 'codeLargeStrong',
+  text: 'Code Large Strong'
+};
+
+export const CodeMedium = Template.bind({});
+CodeMedium.args = {
+  variant: 'codeMedium',
+  text: 'Code Medium'
+};
+
+export const CodeMediumStrong = Template.bind({});
+CodeMediumStrong.args = {
+  variant: 'codeMediumStrong',
+  text: 'Code Medium Strong'
+};
+
+export const CodeSmall = Template.bind({});
+CodeSmall.args = {
+  variant: 'codeSmall',
+  text: 'Code Small'
+};
+
+export const CodeSmallStrong = Template.bind({});
+CodeSmallStrong.args = {
+  variant: 'codeSmallStrong',
+  text: 'Code Small Strong'
+};
+
+export const CodeSmallChip = Template.bind({});
+CodeSmallChip.args = {
+  variant: 'codeSmallChip',
+  text: 'Code Small Chip'
+};
+
+export const Charts = Template.bind({});
+Charts.args = { variant: 'charts', text: 'Charts' };
