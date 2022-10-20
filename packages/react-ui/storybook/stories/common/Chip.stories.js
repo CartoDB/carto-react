84bd9ddc195069861f8340c95c61ee6c67f09ad4
vmilan <vmilan@cartodb.com>
2022-10-20 22:02:19 +0200

    replace figma links

--- packages/react-ui/storybook/stories/common/Chip.stories.js
+++ packages/react-ui/storybook/stories/common/Chip.stories.js
@@ -67,6 +67,12 @@ const options = {
         options: ['default', 'outlined']
       }
     }
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/nmaoLeo69xBJCHm9nc6lEV/CARTO-Components-1.0?node-id=669%3A21447'
+    }
   }
 };
 export default options;
