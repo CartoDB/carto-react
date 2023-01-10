463304a94b769f502ff6a7a15386f4f8b615ab39
Felix Palmer <felixpalmer@gmail.com>
2023-01-10 16:15:55 +0100

    Add FeaturesDroppedLoader to extract header value

---
+++ packages/react-api/src/hooks/FeaturesDroppedLoader.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'FeaturesDroppedLoader',
+  module: 'carto',
+  category: 'geometry',
+  extensions: [],
+  // By only specifying `carto-vector-tile` the SpatialIndexLayer will not use this loader
+  mimeTypes: ['application/vnd.carto-vector-tile'],
+  parse: async (arrayBuffer, options, context) => {
+    const isDroppingFeatures = context.response.headers['features-dropped-from-tile'];
+    const result = await context.parse(arrayBuffer, options, context);
+    return result ? { ...result, isDroppingFeatures } : null;
+  }
+};
