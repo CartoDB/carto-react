8ba2fc208c49e7757af9ea476257a2985a3f9859
vmilan <vmilan@cartodb.com>
2023-01-12 18:19:00 +0100

    autocomplete fix

--- packages/react-ui/src/theme/sections/components/forms.js
+++ packages/react-ui/src/theme/sections/components/forms.js
@@ -481,6 +481,10 @@ export const formsOverrides = {
 
   // Autocomplete
   MuiAutocomplete: {
+    defaultProps: {
+      popupIcon: <ArrowDropIcon />
+    },
+
     styleOverrides: {
       inputRoot: {
         '&[class*="MuiOutlinedInput-root"]': {
@@ -491,7 +495,11 @@ export const formsOverrides = {
         '& .MuiAutocomplete-endAdornment': {
           top: 0,
           right: getSpacing(0.25)
-        }
+        },
+        '& .MuiAutocomplete-popupIndicator:hover, & .MuiAutocomplete-popupIndicator:focus-visible':
+          {
+            backgroundColor: 'transparent'
+          }
       }
     }
   },
