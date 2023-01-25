c1613e2f4eab7390f6433b2c97ec23a514aa7c04
Verónica Milán <vmilan@cartodb.com>
2023-01-25 11:48:02 +0100

    Adjustments in theme to fix uses cases in cloud-native (#585)

--- packages/react-ui/src/theme/sections/components/feedback.js
+++ packages/react-ui/src/theme/sections/components/feedback.js
@@ -1,7 +1,7 @@
 export const feedbackOverrides = {
   // SnackBar
-  MuiSnackBar: {
-    DefaultProps: {
+  MuiSnackbar: {
+    defaultProps: {
       anchorOrigin: {
         vertical: 'bottom',
         horizontal: 'center'
