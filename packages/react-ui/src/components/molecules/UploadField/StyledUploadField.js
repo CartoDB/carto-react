8c6d842775136b69e6b79da77f8fd21d36ea453b
Víctor Velarde <victor.velarde@gmail.com>
2023-03-13 22:05:14 +0100

    Refactor to split styles into external styled component for UploadField

---
+++ packages/react-ui/src/components/molecules/UploadField/StyledUploadField.js
@@ -0,0 +1,46 @@
+import { styled, TextField } from '@mui/material';
+
+const StyledUploadField = styled(TextField, {
+  shouldForwardProp: (prop) => prop !== 'focused'
+})(({ focused, theme }) => ({
+  '& .MuiInputBase-root': {
+    cursor: 'pointer',
+    paddingRight: theme.spacing(1),
+
+    '& input': {
+      cursor: 'pointer'
+    },
+    '&.Mui-disabled': {
+      pointerEvents: 'none',
+
+      '& .MuiButtonBase-root': {
+        color: theme.palette.text.disabled
+      }
+    },
+    '&.MuiInputBase-sizeSmall': {
+      paddingRight: theme.spacing(0.5)
+    }
+  },
+  '& .MuiFormLabel-root': {
+    cursor: 'pointer',
+
+    '&.Mui-disabled': {
+      pointerEvents: 'none'
+    }
+  },
+  ...(focused && {
+    '& .MuiOutlinedInput-root, & .MuiFilledInput-root': {
+      backgroundColor: theme.palette.background.paper
+    },
+    '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline, & .MuiInputBase-root.MuiFilledInput-root::after':
+      {
+        opacity: 1,
+        border: `2px solid ${theme.palette.primary.main}`
+      },
+    '& .MuiFormLabel-root': {
+      color: theme.palette.primary.main
+    }
+  })
+}));
+
+export default StyledUploadField;
--- packages/react-ui/src/components/molecules/UploadField/UploadField.js
+++ packages/react-ui/src/components/molecules/UploadField/UploadField.js
@@ -1,59 +1,13 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import { TextField } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
 
 import FilesIndicator from './FilesIndicator';
 import useFileUpload from './useFileUpload';
-
-const useStyles = makeStyles((theme) => ({
-  uploadField: {
-    '& .MuiInputBase-root': {
-      cursor: 'pointer',
-      paddingRight: theme.spacing(1),
-
-      '& input': {
-        cursor: 'pointer'
-      },
-      '&.Mui-disabled': {
-        pointerEvents: 'none',
-
-        '& .MuiButtonBase-root': {
-          color: theme.palette.text.disabled
-        }
-      },
-      '&.MuiInputBase-sizeSmall': {
-        paddingRight: theme.spacing(0.5)
-      }
-    },
-    '& .MuiFormLabel-root': {
-      cursor: 'pointer',
-
-      '&.Mui-disabled': {
-        pointerEvents: 'none'
-      }
-    }
-  },
-
-  focused: {
-    '& .MuiOutlinedInput-root, & .MuiFilledInput-root': {
-      backgroundColor: theme.palette.background.paper
-    },
-    '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline, & .MuiInputBase-root.MuiFilledInput-root::after':
-      {
-        opacity: 1,
-        border: `2px solid ${theme.palette.primary.main}`
-      },
-    '& .MuiFormLabel-root': {
-      color: theme.palette.primary.main
-    }
-  }
-}));
+import StyledUploadField from './StyledUploadField';
 
 function UploadField({ buttonText, accept, files, ...props }) {
   const { onChange, multiple, placeholder, size, error } = props;
   const uploadInputRef = useRef(null);
-  const classes = useStyles();
 
   const { filesText, getPlaceholder, dragOver, inputEvents, handleFiles, handleReset } =
     useFileUpload({
@@ -66,12 +20,12 @@ function UploadField({ buttonText, accept, files, ...props }) {
 
   return (
     <>
-      <TextField
+      <StyledUploadField
         {...props}
         placeholder={getPlaceholder}
         value={filesText}
         error={error}
-        className={`${classes.uploadField} ${dragOver && classes.focused}`}
+        focused={dragOver}
         InputProps={{
           ...inputEvents,
           readOnly: true,
