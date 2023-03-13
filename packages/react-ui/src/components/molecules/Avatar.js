95c9f25d2beab1085d890bf7978341147d537b06
Verónica Milán <vmilan@cartodb.com>
2023-03-13 15:28:52 +0100

    second  approach with emotion styled

--- packages/react-ui/src/components/molecules/Avatar.js
+++ packages/react-ui/src/components/molecules/Avatar.js
@@ -2,6 +2,9 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import { Avatar as MuiAvatar } from '@mui/material';
 import { css, styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/system';
+import { themeTypography } from '../../theme/sections/typography';
+import { getSpacing } from '../../theme/themeUtils';
 
 const Sizes = {
   large: 5,
@@ -9,6 +12,55 @@ const Sizes = {
   small: 3,
   xsmall: 2.25
 };
+
+const avatarTheme = createTheme({
+  components: {
+    AvatarWrapper: {
+      styleOverrides: {
+        root: {
+          color: 'darkslategray'
+        },
+        large: {
+          ...themeTypography.h6
+        },
+        medium: {
+          ...themeTypography.subtitle1
+        },
+        small: {
+          ...themeTypography.caption,
+          fontWeight: 500
+        },
+        xsmall: {
+          ...themeTypography.caption,
+          fontWeight: 500,
+
+          '& svg': {
+            width: getSpacing(2),
+            height: getSpacing(2)
+          }
+        }
+      }
+    }
+  }
+});
+
+const AvatarWrapper = styled('div', {
+  // Configure which props should be forwarded on DOM
+  shouldForwardProp: (prop) => prop !== 'size' && prop !== 'sx',
+  name: 'AvatarWrapper',
+  slot: 'Root',
+  // We are specifying here how the styleOverrides are being applied based on props
+  overridesResolver: (props, styles) => [
+    styles.root,
+    props.size === 'large' && styles.large,
+    props.size === 'medium' && styles.medium,
+    props.size === 'small' && styles.small,
+    props.size === 'xsmall' && styles.xsmall
+  ]
+})(({ theme }) => ({
+  backgroundColor: 'aliceblue',
+  padding: theme.spacing(1)
+}));
 
 const handleSizeStyles = (size) => {
   switch (size) {
@@ -54,9 +106,15 @@ const Avatar = forwardRef(({ size, children, ...otherProps }, ref) => {
   // https://mui.com/material-ui/guides/composition/#caveat-with-refs
 
   return (
-    <AvatarContainer {...otherProps} ref={ref} size={size}>
-      {children}
-    </AvatarContainer>
+    <>
+      <AvatarContainer {...otherProps} ref={ref} size={size}>
+        {children}
+      </AvatarContainer>
+      <ThemeProvider theme={avatarTheme}>
+        <AvatarWrapper size={size}>Primary</AvatarWrapper>
+        <AvatarWrapper size={size}>Secondary</AvatarWrapper>
+      </ThemeProvider>
+    </>
   );
 });
 
