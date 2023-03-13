71493d97039885303b291b942154bbf76bd922c6
Víctor Velarde <victor.velarde@gmail.com>
2023-03-13 20:57:00 +0100

    Import new function from public api at package

--- packages/react-ui/storybook/stories/molecules/Avatar.stories.js
+++ packages/react-ui/storybook/stories/molecules/Avatar.stories.js
@@ -3,7 +3,7 @@ import { Avatar, Grid, Box, useTheme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { Star } from '@mui/icons-material';
 import Typography from '../../../src/components/atoms/Typography';
-import { getCartoColorPropsForItem } from '../../../src/utils/palette';
+import { getCartoColorPropsForItem } from '@carto/react-ui';
 
 const options = {
   title: 'Molecules/Avatar',
