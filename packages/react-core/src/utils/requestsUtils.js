715da90bb297d47ee720874dcb423350fed75bbb
Víctor Velarde <victor.velarde@gmail.com>
2023-03-27 16:46:32 +0200

    Allow to inject headers externally to executeSQL helper

--- packages/react-core/src/utils/requestsUtils.js
+++ packages/react-core/src/utils/requestsUtils.js
@@ -7,13 +7,15 @@ export const REQUEST_GET_MAX_URL_LENGTH = 2048;
  * Simple GET request
  */
 export function getRequest(url, opts, headers = {}) {
+  const { headers: extraHeaders, ...extraOpts } = opts;
   return new Request(url, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
-      ...headers
+      ...headers,
+      ...extraHeaders
     },
-    ...opts
+    ...extraOpts
   });
 }
 
@@ -21,15 +23,17 @@ export function getRequest(url, opts, headers = {}) {
  * Simple POST request
  */
 export function postRequest(url, payload, opts, headers = {}) {
+  const { headers: extraHeaders, ...extraOpts } = opts;
   return new Request(url, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      ...headers
+      ...headers,
+      ...extraHeaders
     },
     body: JSON.stringify(payload),
-    ...opts
+    ...extraOpts
   });
 }
 
