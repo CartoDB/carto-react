61bfccafa8384f8a2e85d699c5f4d46b6bdd7dfa
Janto <jantolg@gmail.com>
2023-04-10 13:31:42 +0200

    feat: change makestyles to styled component Note component

--- packages/react-ui/src/widgets/legend/Note.js
+++ packages/react-ui/src/widgets/legend/Note.js
@@ -1,16 +1,13 @@
 import { Box } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
+import { styled } from '@mui/material/styles';
 import React from 'react';
 import Typography from '../../components/atoms/Typography';
 
-const useNoteStyles = makeStyles(() => ({
-  note: {
-    fontWeight: 'normal'
-  }
+const LighterTypography = styled(Typography)(({ theme }) => ({
+  fontWeight: 'normal'
 }));
 
 export default function Note({ children }) {
-  const classes = useNoteStyles();
 
   if (!children) {
     return null;
@@ -19,9 +16,9 @@ export default function Note({ children }) {
   return (
     <Box mt={1} data-testid='note-legend'>
       <Typography variant='caption'>Note:</Typography>{' '}
-      <Typography className={classes.note} variant='caption'>
+      <LighterTypography variant='caption'>
         {children}
-      </Typography>
+      </LighterTypography>
     </Box>
   );
 }
