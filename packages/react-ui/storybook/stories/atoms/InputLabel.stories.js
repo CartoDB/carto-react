bc652db9a274875a0e2d63bcbb0514c69dcc4f30
Verónica Milán <vmilan@cartodb.com>
2023-04-13 18:36:12 +0200

    hacky geocoder

--- packages/react-ui/storybook/stories/atoms/InputLabel.stories.js
+++ packages/react-ui/storybook/stories/atoms/InputLabel.stories.js
@@ -1,7 +1,49 @@
 import React from 'react';
-import { FormControl, InputLabel, OutlinedInput } from '@mui/material';
+import {
+  Box,
+  CircularProgress,
+  FormControl,
+  InputBase,
+  InputLabel,
+  OutlinedInput,
+  Paper,
+  SvgIcon,
+  styled
+} from '@mui/material';
 import LabelWithIndicator from '../../../src/components/atoms/LabelWithIndicator';
 import Typography from '../../../src/components/atoms/Typography';
+
+const StyledPaper = styled(Paper)(({ theme: { spacing } }) => ({
+  height: spacing(4.5),
+  width: spacing(30),
+  paddingLeft: spacing(1.5),
+  borderRadius: spacing(0.5),
+  display: 'flex',
+  alignItems: 'center'
+}));
+
+const IconWrapper = styled('div')(({ theme: { palette, typography } }) => ({
+  display: 'flex',
+
+  '& svg': {
+    fill: palette.text.secondary
+  }
+}));
+
+const InputSearch = styled(InputBase)(({ theme: { typography, spacing } }) => ({
+  //...typography.body2,
+  width: `calc(100% - ${spacing(5)})`,
+  marginLeft: spacing(1)
+}));
+
+const SearchIcon = (args) => (
+  <SvgIcon {...args} color='textSecondary'>
+    <path
+      d='M11,4 C14.8659932,4 18,7.13400675 18,11 C18,12.7003211 17.3937669,14.2590489 16.3856562,15.4718279 L19.4748737,18.5606602 L18.0606602,19.9748737 L14.8998887,16.8138615 C13.7854137,17.5629194 12.4437497,18 11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 Z M11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 Z'
+      fill='inherit'
+    ></path>
+  </SvgIcon>
+);
 
 const options = {
   title: 'Atoms/Input Label',
@@ -41,10 +83,30 @@ const Template = ({ label, ...args }) => {
 
 const CompositionTemplate = ({ label, ...args }) => {
   return (
-    <FormControl {...args} size='small'>
-      <InputLabel>{label}</InputLabel>
-      <OutlinedInput placeholder='Input text' />
-    </FormControl>
+    <>
+      <Box mb={15}>
+        <Typography variant='subtitle1' mb={4}>
+          {'Geocoder widget UI'}
+        </Typography>
+        <StyledPaper elevation={2}>
+          <IconWrapper>
+            {false ? <CircularProgress size={18} /> : <SearchIcon />}
+          </IconWrapper>
+
+          <InputSearch
+            type='search'
+            tabIndex={-1}
+            size='small'
+            placeholder='Search address'
+            inputProps={{ 'aria-label': 'GeocoderSearch' }}
+          />
+        </StyledPaper>
+      </Box>
+      <FormControl {...args} size='small'>
+        <InputLabel>{label}</InputLabel>
+        <OutlinedInput placeholder='Input text' />
+      </FormControl>
+    </>
   );
 };
 
