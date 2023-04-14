03690251d1aedf6a7f8178740ea56dd3a931636e
Janto <jantolg@gmail.com>
2023-04-14 12:33:33 +0200

    chore: refactor chip.stories from makestyles to styled-component

---
+++ packages/react-ui/storybook/stories/common.stories.styled.js
@@ -0,0 +1,29 @@
+import {
+  Box,
+  styled
+} from '@mui/material';
+import Typography from '../../src/components/atoms/Typography';
+
+export const Container = styled(Box)(({theme}) => ({
+  display: 'flex',
+  alignItems: 'center',
+  marginTop: theme.spacing(4)
+}));
+
+export const Label = styled(Typography)(({theme}) => ({
+  minWidth: theme.spacing(25)
+}));
+
+export const Header = styled(Label)(({theme}) => ({
+  marginTop: theme.spacing(4)
+}));
+
+export const ChipWrapper = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'small'
+})(({theme, small}) => ({
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: theme.spacing(1),
+  overflow: 'auto',
+  height: theme.spacing(small ? 3 : 4)
+}));
