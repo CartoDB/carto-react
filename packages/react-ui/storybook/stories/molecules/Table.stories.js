fc2f0270afbcdafa645acbedc0a48809cc542283
Juan D. Jara <juanorigami@gmail.com>
2023-04-24 12:13:07 +0200

    remove paper component wrapper

--- packages/react-ui/storybook/stories/molecules/Table.stories.js
+++ packages/react-ui/storybook/stories/molecules/Table.stories.js
@@ -6,7 +6,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Chip, FormControl, MenuItem, Select, Tooltip, Typography } from '@mui/material';
+import {
+  Box,
+  Chip,
+  FormControl,
+  MenuItem,
+  Select,
+  Tooltip,
+  Typography
+} from '@mui/material';
 
 function createData(
   name, // : string,
@@ -73,7 +81,7 @@ export default options;
 
 const PlaygroundTemplate = (args) => {
   return (
-    <TableContainer component={Paper} sx={{ maxWidth: '100%' }}>
+    <TableContainer sx={{ maxWidth: '100%' }}>
       <Table aria-label='simple table'>
         <TableHead>
           <TableRow>
@@ -144,7 +152,7 @@ export const Playground = PlaygroundTemplate.bind({});
 Playground.args = {};
 
 const ScrollTemplate = ({ maxHeight, maxWidth }) => (
-  <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+  <Box sx={{ width: '100%', overflow: 'hidden' }}>
     <TableContainer sx={{ maxWidth, maxHeight }}>
       <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
@@ -209,7 +217,7 @@ const ScrollTemplate = ({ maxHeight, maxWidth }) => (
         </TableBody>
       </Table>
     </TableContainer>
-  </Paper>
+  </Box>
 );
 
 export const Scroll = ScrollTemplate.bind(
