6c5fe754024c8938a8150ef610f8b4bcbdfce3f0
Juan D. Jara <juanorigami@gmail.com>
2023-04-25 10:12:40 +0200

    remove unused

--- packages/react-ui/storybook/stories/molecules/Table.stories.js
+++ packages/react-ui/storybook/stories/molecules/Table.stories.js
@@ -5,7 +5,6 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import {
   Box,
   Chip,
@@ -16,23 +15,6 @@ import {
   Typography
 } from '@mui/material';
 
-function createData(
-  name, // : string,
-  calories, // : number,
-  fat, // : number,
-  carbs, // : number,
-  protein // : number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-// const rows = [
-//   createData('Frozen yoghurt', 'very long text that should trigger the overflow style', 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 'very long text that should trigger the overflow style'),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
 const rows = [
   { name: 'Test Data 1', type: 'string', mode: '', description: 'Test Data 1' },
   {
