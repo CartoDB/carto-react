346d93577e93d9836101f6c1a5f6e2b7c954e1db
Juan D. Jara <juanorigami@gmail.com>
2023-04-25 12:43:49 +0200

    group imports

--- packages/react-ui/storybook/stories/molecules/Table.stories.js
+++ packages/react-ui/storybook/stories/molecules/Table.stories.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 import {
   Box,
   Chip,
@@ -12,7 +6,13 @@ import {
   MenuItem,
   Select,
   Tooltip,
-  Typography
+  Typography,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableContainer,
+  TableBody,
+  Table
 } from '@mui/material';
 
 const rows = [
