bbf2681e7acd2df54f6fec2c6ab25e90f633af5f
Verónica Milán <vmilan@cartodb.com>
2023-05-16 16:09:03 +0200

    Remove ReactDOMServer dependency and simplify avatar image fallback

--- packages/react-ui/src/theme/sections/components/dataDisplay.js
+++ packages/react-ui/src/theme/sections/components/dataDisplay.js
@@ -1,15 +1,14 @@
-import React from 'react';
 import { ICON_SIZE_MEDIUM, ICON_SIZE_LARGE, ICON_SIZE_SMALL } from '../../themeConstants';
 import { getSpacing } from '../../themeUtils';
 import { commonPalette } from '../palette';
 import { themeTypography } from '../typography';
-import { PersonOutline } from '@mui/icons-material';
-import { getIconPath } from '../../themeUtils';
 
 const tooltipArrowSize = 1;
 const tooltipSeparation = 0.5;
 const tooltipMargin = tooltipArrowSize + tooltipSeparation;
-const avatarFallbackImage = getIconPath(<PersonOutline />);
+// PersonOutlined path: https://github.com/mui/material-ui/blob/master/packages/mui-icons-material/material-icons/person_outline_24px.svg?short_path=edcd654
+const avatarFallbackImage =
+  'M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z';
 const avatarCircularRadius = '50%';
 const avatarRoundedRadius = 0.5;
 
