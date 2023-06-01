93c7c7f440150907cb4cdbcf227ee14154bbee00
Verónica Milán <vmilan@cartodb.com>
2023-06-01 18:16:17 +0200

    removed unneeded TablePaginationActions types

--- packages/react-ui/src/components/molecules/Table/TablePaginationActions.d.ts
+++
@@ -1,8 +0,0 @@
-import { TablePaginationActionsProps as MuiTablePaginationActionsProps } from '@mui/material/TablePagination/TablePaginationActions';
-
-export type TablePaginationActionsProps = MuiTablePaginationActionsProps & {
-  lastPageTooltip?: string;
-};
-
-declare const TablePaginationActions: (props: TablePaginationActionsProps) => JSX.Element;
-export default TablePaginationActions;
