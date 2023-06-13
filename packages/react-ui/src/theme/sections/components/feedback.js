e70075fefc62186a61776f037f938b22dc3650ab
Benjamin Becquet <benjamin.becquet@gmail.com>
2023-06-13 14:05:20 +0200

    Add custom <Alert> component (#698)

--- packages/react-ui/src/theme/sections/components/feedback.js
+++ packages/react-ui/src/theme/sections/components/feedback.js
@@ -1,3 +1,5 @@
+import { ICON_SIZE_MEDIUM } from '../../themeConstants';
+
 export const feedbackOverrides = {
   // SnackBar
   MuiSnackbar: {
@@ -30,6 +32,34 @@ export const feedbackOverrides = {
         '&::after': {
           background: `linear-gradient( 90deg, transparent, ${theme.palette.black[4]}, transparent )`
         }
+      })
+    }
+  },
+
+  // Alert
+  MuiAlert: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        borderRadius: theme.spacing(1),
+        alignSelf: 'start',
+        width: '100%'
+      }),
+      icon: ({ theme }) => ({
+        svg: {
+          width: ICON_SIZE_MEDIUM,
+          height: ICON_SIZE_MEDIUM
+        }
+      })
+    }
+  },
+
+  // AlertTitle
+  MuiAlertTitle: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        ...theme.typography.body2,
+        fontWeight: theme.typography.fontWeightBold,
+        color: 'inherit'
       })
     }
   }
