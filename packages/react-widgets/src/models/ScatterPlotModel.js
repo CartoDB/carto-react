4c5a0dd22500d7af28bfd46c6cad010695492ba5
Stefano Pettini <spettini@cartodb.com>
2023-06-14 12:02:14 +0200

    Support for remote scatter plot widget

--- packages/react-widgets/src/models/ScatterPlotModel.js
+++ packages/react-widgets/src/models/ScatterPlotModel.js
@@ -1,8 +1,12 @@
+import { _executeModel } from '@carto/react-api';
 import { Methods, executeTask } from '@carto/react-workers';
-import { wrapModelCall } from './utils';
+import { normalizeObjectKeys, wrapModelCall } from './utils';
+
+// Make sure this is sync with the same constant in cloud-native/maps-api
+export const HARD_LIMIT = 1000;
 
 export function getScatter(props) {
-  return wrapModelCall(props, fromLocal);
+  return wrapModelCall(props, fromLocal, fromRemote);
 }
 
 function fromLocal(props) {
@@ -17,4 +21,29 @@ function fromLocal(props) {
     yAxisColumn,
     yAxisJoinOperation
   });
+}
+
+function formatResult(res) {
+  return res.map(({ x, y }) => [x, y]);
+}
+
+function fromRemote(props) {
+  const { source, spatialFilter, abortController, ...params } = props;
+  const { xAxisColumn, xAxisJoinOperation, yAxisColumn, yAxisJoinOperation } = params;
+
+  return _executeModel({
+    model: 'scatterplot',
+    source,
+    spatialFilter,
+    params: {
+      xAxisColumn,
+      xAxisJoinOperation,
+      yAxisColumn,
+      yAxisJoinOperation,
+      limit: HARD_LIMIT
+    },
+    opts: { abortController }
+  })
+    .then((res) => normalizeObjectKeys(res.rows))
+    .then(formatResult);
 }
