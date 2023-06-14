da240ca709b2ae8eccddc035913e9b2be526b08c
Stefano Pettini <spettini@cartodb.com>
2023-06-14 12:33:41 +0000

    Reduced limit to 500

--- packages/react-widgets/src/models/ScatterPlotModel.js
+++ packages/react-widgets/src/models/ScatterPlotModel.js
@@ -3,7 +3,7 @@ import { Methods, executeTask } from '@carto/react-workers';
 import { normalizeObjectKeys, wrapModelCall } from './utils';
 
 // Make sure this is sync with the same constant in cloud-native/maps-api
-export const HARD_LIMIT = 1000;
+export const HARD_LIMIT = 500;
 
 export function getScatter(props) {
   return wrapModelCall(props, fromLocal, fromRemote);
