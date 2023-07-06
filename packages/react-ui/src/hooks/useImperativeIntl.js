833409b9dcc213f4193c5e0559dd2df8078bf50c
menusal <daniarlandis@gmail.com>
2023-07-06 17:00:10 +0200

    add hook to use imperative API from Intl

---
+++ packages/react-ui/src/hooks/useImperativeIntl.js
@@ -0,0 +1,24 @@
+import { createIntl, createIntlCache } from 'react-intl';
+import { messages } from '../localization';
+
+// This is optional but highly recommended
+// since it prevents memory leak
+const cache = createIntlCache();
+
+/**
+ * The hook returns an instance of the Intl object with the specified locale
+ * and messages using imperative API.
+ * @see https://formatjs.io/docs/react-intl/api/#createintl
+ * @param {string} locale - The locale to use.
+ * @returns the `intl` object.
+ */
+export default function useImperativeIntl({ locale }) {
+  const intl = createIntl(
+    {
+      locale: locale,
+      messages: messages[locale]
+    },
+    cache
+  );
+  return intl;
+}
