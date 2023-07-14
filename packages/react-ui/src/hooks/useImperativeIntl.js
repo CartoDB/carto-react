1a28609a0d96ed9a69fa10e8c2dc32e0c85e5b5b
menusal <daniarlandis@gmail.com>
2023-07-14 13:44:31 +0200

    wip override internal from external provider

--- packages/react-ui/src/hooks/useImperativeIntl.js
+++ packages/react-ui/src/hooks/useImperativeIntl.js
@@ -1,18 +1,20 @@
-import { IntlContext, createIntl, createIntlCache } from 'react-intl';
+import { IntlContext, createIntl, createIntlCache, useIntl } from 'react-intl';
 import { messages } from '../localization';
-import { createContext, useContext, useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 import { match } from '@formatjs/intl-localematcher';
 
 export const DEFAULT_LOCALE = 'en-US';
 const cache = createIntlCache();
 
-// TODO: External intl
-const I18nContext = createContext(IntlContext);
+// TODO: Pending to get external intl provider
 
 export default function useImperativeIntl() {
+  const extenal = useIntl();
+  console.log('useImperativeIntl', extenal);
   // TODO: External intl
-  const externalIntl = useContext(I18nContext);
-  // @ts-ignore
+  const externalIntl = useContext(IntlContext);
+  console.log('findMatchingMessagesLocale', externalIntl);
+
   const locale = externalIntl?.locale || DEFAULT_LOCALE;
   const messagesLocale = findMatchingMessagesLocale(locale);
 
