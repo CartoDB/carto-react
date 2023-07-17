7fd071cf356f0cf450f4bc882c28864735e9b108
Verónica Milán <vmilan@cartodb.com>
2023-07-17 13:03:31 +0200

    fix types

--- packages/react-ui/src/components/atoms/SelectField.d.ts
+++ packages/react-ui/src/components/atoms/SelectField.d.ts
@@ -1,14 +1,14 @@
-import { SelectProps } from '@mui/material/Select';
+import { SelectChangeEvent, SelectProps } from '@mui/material/Select';
 import { TextFieldProps } from '@mui/material/TextField';
 import React from 'react';
 
-export type SelectFieldProps = SelectProps &
+export type SelectFieldProps = Omit<SelectProps, 'placeholder'> &
   Omit<TextFieldProps, 'placeholder'> & {
     children?: React.ReactNode;
-    onChange?: Function;
+    onChange?: (event: SelectChangeEvent<string[]>, child: React.ReactNode) => void;
     placeholder?: React.ReactNode;
     size?: 'small' | 'medium';
-    customRenderValue?: (value) => React.ReactNode;
+    customRenderValue?: (value: string[]) => React.ReactNode;
   };
 
 declare const SelectField: (props: SelectFieldProps) => JSX.Element;
