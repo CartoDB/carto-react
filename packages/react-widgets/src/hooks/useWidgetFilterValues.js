f89589fee67ef141b20f088bd61f731132a163e6
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-05 17:46:03 +0200

    TimeSeriesWidget: support for splitByCategory, multiple series and stepMultiplier

--- packages/react-widgets/src/hooks/useWidgetFilterValues.js
+++ packages/react-widgets/src/hooks/useWidgetFilterValues.js
@@ -8,13 +8,14 @@ import { useMemo } from 'react';
  * @param  {object} props
  * @param  {string} props.dataSource - ID of the source to get the filters from.
  * @param  {string} props.id - ID of the widget that applied the filter.
- * @param  {string} props.column - name of column of this widget.
+ * @param  {string=} props.column - name of column of this widget.
  * @param  {string} props.type - type of filter
  */
 export function useWidgetFilterValues({ dataSource, id, column, type }) {
   const { filters } = useSelector((state) => selectSourceById(state, dataSource) || {});
 
   return useMemo(() => {
+    if (!column) return null
     const filter = filters?.[column]?.[type];
     if (!filter || filter.owner !== id) {
       return null;
--- packages/react-widgets/src/models/TimeSeriesModel.js
+++ packages/react-widgets/src/models/TimeSeriesModel.js
@@ -1,9 +1,27 @@
 import { _executeModel } from '@carto/react-api/';
 import { Methods, executeTask } from '@carto/react-workers';
 import { normalizeObjectKeys, wrapModelCall } from './utils';
+import { AggregationTypes } from '@carto/react-core';
 
 export function getTimeSeries(props) {
-  return wrapModelCall(props, fromLocal, fromRemote);
+  if (props.series) {
+    const { series, propsNoSeries } = props;
+    return Promise.all(
+      series.map(({ operation, operationColumn }) => {
+        const category = getCategory(operation, operationColumn, series);
+        return {
+          data: wrapModelCall(
+            { ...propsNoSeries, operation, operationColumn },
+            fromLocal,
+            fromRemote
+          ),
+          category
+        };
+      })
+    );
+  } else {
+    return wrapModelCall(props, fromLocal, fromRemote);
+  }
 }
 
 // From local
@@ -13,23 +31,41 @@ function fromLocal({
   operationColumn,
   joinOperation,
   operation,
-  stepSize
+  stepSize,
+  stepMultiplier,
+  splitByCategory,
+  splitByCategoryLimit,
+  splitByCategoryValues
 }) {
   return executeTask(source.id, Methods.FEATURES_TIME_SERIES, {
     filters: source.filters,
     filtersLogicalOperator: source.filtersLogicalOperator,
     column,
     stepSize,
+    stepMultiplier,
     operationColumn: operationColumn || column,
     joinOperation,
-    operation
+    operation,
+    splitByCategory,
+    splitByCategoryLimit,
+    splitByCategoryValues
   });
 }
 
 // From remote
 function fromRemote(props) {
   const { source, abortController, spatialFilter, ...params } = props;
-  const { column, operationColumn, joinOperation, operation, stepSize } = params;
+  const {
+    column,
+    operationColumn,
+    joinOperation,
+    operation,
+    stepSize,
+    stepMultiplier,
+    splitByCategory,
+    splitByCategoryLimit,
+    splitByCategoryValues
+  } = params;
 
   return _executeModel({
     model: 'timeseries',
@@ -38,10 +74,28 @@ function fromRemote(props) {
     params: {
       column,
       stepSize,
+      stepMultiplier,
       operationColumn: operationColumn || column,
       joinOperation,
-      operation
+      operation,
+      splitByCategory,
+      splitByCategoryLimit,
+      splitByCategoryValues
     },
     opts: { abortController }
   }).then((res) => normalizeObjectKeys(res.rows));
+}
+
+function getCategory(series, operation, operationColumn) {
+  const countColumnUsed = series.filter(
+    (s) => s.operationColumn === operationColumn
+  ).length;
+  if (countColumnUsed < 2) {
+    return operationColumn;
+  } else {
+    if (operation === AggregationTypes.COUNT) {
+      return `count of records`;
+    }
+    return `${operation} of ${operationColumn}`; // todo translate maybe ?
+  }
 }
