629f136a35f458563d4712a92636cedd15be9308
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-06 15:22:27 +0200

    use common model for multiple series

--- packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
+++ packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
@@ -188,39 +188,45 @@ function TimeSeriesWidgetUIContent({
     const categories = [];
     const colorMapping = {};
 
-    const firstSerieEntry = data[0];
-    if (
-      firstSerieEntry &&
-      Array.isArray(firstSerieEntry.data) &&
-      firstSerieEntry.category
-    ) {
-      // multiple series model
-      // array of objects {data, category}
-      for (const { data: seriesDataRaw, category } of data) {
+    // TODO: decide if we use common model for multiple series and splitByCategory
+    // as `splitByCategory` is less optimal and with common model we have "join" and then "split" data
+    // why common model?
+    // some other commponents are relying on old layout of data
+
+    // const firstSerieEntry = data[0];
+    // if (
+    //   firstSerieEntry &&
+    //   Array.isArray(firstSerieEntry.data) &&
+    //   firstSerieEntry.category
+    // ) {
+    //   // multiple series model
+    //   // array of objects {data, category}
+    //   for (const { data: seriesDataRaw, category } of data) {
+    //     categories.push(category);
+    //     series.push({
+    //       category,
+    //       data: seriesDataRaw.map(({ name, value }) => [name, value])
+    //     });
+    //   }
+    // } else {
+
+    // splitByCategory model
+    // one array, with category embedded: { name: 1009843200000, category: 'DECEPTIVE PRACTICE', value: 6 }
+    for (const { name, value, category } of data) {
+      let dataSeriesIndex = category ? categories.indexOf(category) : 0;
+      if (dataSeriesIndex === -1) {
+        dataSeriesIndex = categories.length;
         categories.push(category);
-        series.push({
+      }
+      if (!series[dataSeriesIndex]) {
+        series[dataSeriesIndex] = {
           category,
-          data: seriesDataRaw.map(({ name, value }) => [name, value])
-        });
+          data: []
+        };
       }
-    } else {
-      // splitByCategory model
-      // one array, with category embedded: { name: 1009843200000, category: 'DECEPTIVE PRACTICE', value: 6 }
-      for (const { name, value, category } of data) {
-        let dataSeriesIndex = category ? categories.indexOf(category) : 0;
-        if (dataSeriesIndex === -1) {
-          dataSeriesIndex = categories.length;
-          categories.push(category);
-        }
-        if (!series[dataSeriesIndex]) {
-          series[dataSeriesIndex] = {
-            category,
-            data: []
-          };
-        }
-        series[dataSeriesIndex].data.push([name, value]);
-      }
+      series[dataSeriesIndex].data.push([name, value]);
     }
+    // }
 
     series.forEach(({ category }, i) => {
       series[i].color = getColorByCategory(category, {
@@ -379,7 +385,9 @@ function TimeSeriesWidgetUIContent({
     [onSelectedCategoriesChange, selectedCategories]
   );
 
-  const isLegentVisible = showLegend !== undefined ? showLegend : series.length > 1;
+  const isLegendVisible = Boolean(
+    showLegend !== undefined ? showLegend : series.length > 1
+  );
 
   const chart = (
     <TimeSeriesChart
@@ -390,7 +398,7 @@ function TimeSeriesWidgetUIContent({
       tooltip={tooltip}
       formatter={formatter}
       tooltipFormatter={(params) =>
-        tooltipFormatter(params, stepSize, formatter, stepMultiplier, isLegentVisible)
+        tooltipFormatter(params, stepSize, formatter, stepMultiplier, isLegendVisible)
       }
       height={height}
       animation={animation}
@@ -399,7 +407,7 @@ function TimeSeriesWidgetUIContent({
     />
   );
 
-  const legend = isLegentVisible && (
+  const legend = isLegendVisible && (
     <TimeSeriesLegend
       series={series}
       selectedCategories={selectedCategories}
@@ -487,7 +495,10 @@ function TimeSeriesWidgetUIContent({
           {legend}
         </Grid>
       ) : (
-        chart
+        <>
+          {chart}
+          {legend}
+        </>
       )}
     </Box>
   );
