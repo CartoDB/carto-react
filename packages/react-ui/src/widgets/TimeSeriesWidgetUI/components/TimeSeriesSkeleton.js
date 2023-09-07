d377f3bced83039d1cc7f5ce66be3c6289325823
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-07 02:18:58 +0200

    skeleton improvements

--- packages/react-ui/src/widgets/TimeSeriesWidgetUI/components/TimeSeriesSkeleton.js
+++ packages/react-ui/src/widgets/TimeSeriesWidgetUI/components/TimeSeriesSkeleton.js
@@ -1,40 +1,57 @@
 import React from 'react';
 import { Box, Grid, styled } from '@mui/material';
 import { Skeleton } from '@mui/material';
-import { SKELETON_HEIGHT, SkeletonBarsGrid } from '../../SkeletonWidgets';
+import { SkeletonBarsGrid } from '../../SkeletonWidgets';
 import { BREAKPOINTS } from '../../../theme/themeConstants';
 import GraphLine from '../../../assets/images/GraphLine';
+import { getSpacing } from '../../../theme/themeUtils';
 
-const Root = styled(Grid)(({ theme }) => ({
-  alignItems: 'stretch',
+const Root = styled(Grid)(() => ({
   containerType: 'inline-size',
-
-  [`@container (max-width: ${BREAKPOINTS.XS}px)`]: {
-    ' > div': {
-      marginRight: 0
-    }
-  }
+  gap: 0,
+  margin: 0,
+  padding: 0
 }));
 
 const Controls = styled(Grid)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
-  justifyContent: 'space-between',
-  marginRight: theme.spacing(4)
+  justifyContent: 'flex-end',
+  margin: 0,
+  paddingLeft: theme.spacing(1),
+  paddingBottom: theme.spacing(3),
+  gap: theme.spacing(2),
+  [`@container (max-width: ${BREAKPOINTS.XS}px)`]: {
+    paddingLeft: 0
+  }
 }));
 
 const Graph = styled(Grid)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'flex-end',
-  marginBottom: theme.spacing(2)
+  margin: 0,
+  paddingTop: 0,
+  paddingBottom: 0,
+  paddingLeft: theme.spacing(5),
+  [`@container (max-width: ${BREAKPOINTS.XS}px)`]: {
+    paddingLeft: theme.spacing(1)
+  }
+}));
+
+const Legend = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  gap: theme.spacing(2),
+  margin: 0,
+  padding: theme.spacing(2.25, 0.5)
 }));
 
 const SkeletonGraphLine = styled(SkeletonBarsGrid)(({ theme }) => ({
   svg: {
     width: '100%',
     height: 'auto',
-    minHeight: theme.spacing(20),
     paddingTop: theme.spacing(4),
     fontSize: 'initial',
     fill: 'none',
@@ -45,28 +62,42 @@ const SkeletonGraphLine = styled(SkeletonBarsGrid)(({ theme }) => ({
   }
 }));
 
-const TimeSeriesSkeleton = ({ height }) => {
+const TimeSeriesSkeleton = ({ height, showLegend = true }) => {
   return (
-    <Root container height={height || SKELETON_HEIGHT}>
+    <Root container>
+      <Grid item xs={12} pt={1} pb={1}>
+        <Skeleton width={48} height={8} />
+      </Grid>
       <Controls item>
         <Grid item>
-          <Skeleton width={48} height={8} />
-        </Grid>
-
-        <Grid item>
-          {[...Array(3)].map((_, i) => (
-            <Box key={i} mt={2}>
-              <Skeleton width={24} height={24} />
-            </Box>
-          ))}
+          <Box>
+            <Skeleton width={24} height={24} />
+          </Box>
+          <Box mt={2}>
+            <Skeleton width={24} height={24} />
+          </Box>
+          <Box mt={1}>
+            <Skeleton width={24} height={24} />
+          </Box>
         </Grid>
       </Controls>
 
-      <Graph item xs>
-        <SkeletonGraphLine height='80%'>
+      <Graph item xs height={height || getSpacing(22)}>
+        <SkeletonGraphLine>
           <GraphLine preserveAspectRatio='none' />
         </SkeletonGraphLine>
+        <Box display='flex' flexDirection='row' gap='20px' pt={1} pb={1}>
+          <Skeleton width={48} height={8} />
+          <Skeleton width={48} height={8} />
+        </Box>
       </Graph>
+
+      {showLegend && (
+        <Legend item xs={12}>
+          <Skeleton width={48} height={8} />
+          <Skeleton width={48} height={8} />
+        </Legend>
+      )}
     </Root>
   );
 };
