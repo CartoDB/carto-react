297cb0b4adcc06705e57d06f708abf908fb3f4b1
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-10 16:09:51 +0200

    legend: only support clicks in splitByCategory mode

--- packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
+++ packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
@@ -307,7 +307,7 @@ function TimeSeriesWidgetUIContent({
     <TimeSeriesLegend
       series={series}
       selectedCategories={selectedCategories}
-      onCategoryClick={handleCategoryClick}
+      onCategoryClick={onSelectedCategoriesChange && handleCategoryClick}
     />
   );
 
