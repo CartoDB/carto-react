72effab2f0e835d7c8f45f4edbe7100a305918f4
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-13 10:05:04 +0200

    fixing layout again

--- packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
+++ packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
@@ -47,6 +47,7 @@ function TimeSeriesWidgetUI({
   if (isLoading)
     return (
       <TimeSeriesSkeleton
+        fitHeight={fitHeight}
         height={height}
         showControls={showControls}
         showLegend={showLegend}
