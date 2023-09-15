364349e0ee5bf192ba5d4c5cac9648d1cb4fb26e
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-15 12:51:34 +0200

    TableWidget: skeleton match original sizing (dense+height) + CR

--- packages/react-ui/src/utils/palette.js
+++ packages/react-ui/src/utils/palette.js
@@ -66,3 +66,29 @@ export function getCartoColorStylePropsForItem(theme, index) {
     color: theme.palette.getContrastText(colors[chosenIndex])
   };
 }
+
+/**
+ * Get color by category.
+ *
+ * Each new category gets new color from `palette` if specified.
+ *
+ * Palette state is kept in `colorMapping`
+ */
+export function getColorByCategory(category, { palette, fallbackColor, colorMapping }) {
+  const assignedColor = colorMapping[category];
+  if (assignedColor) {
+    return colorMapping[category];
+  }
+
+  if (!palette || palette.length === 0) {
+    return fallbackColor;
+  }
+
+  const nextFreeColorIndex = Math.min(
+    Object.keys(colorMapping).length,
+    palette.length - 1
+  );
+  const newColor = palette[nextFreeColorIndex];
+  colorMapping[category] = newColor;
+  return newColor;
+}
