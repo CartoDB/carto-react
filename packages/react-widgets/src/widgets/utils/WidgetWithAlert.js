e53f69ae4745716baeb1ceacaf6bf015e0eedc48
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-18 15:54:24 +0200

    WidgetWithAlert with stable height for table/timeseries widget

--- packages/react-widgets/src/widgets/utils/WidgetWithAlert.js
+++ packages/react-widgets/src/widgets/utils/WidgetWithAlert.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { checkIfSourceIsDroppingFeature } from '@carto/react-redux';
 import { NoDataAlert } from '@carto/react-ui/';
 import { useSelector } from 'react-redux';
@@ -11,23 +11,36 @@ export default function WidgetWithAlert({
   noDataAlertProps = {},
   warning,
   global = false,
+  stableHeight, // if specified, "no-data" state will attempt to keep the same height as when rendered with data
   children
 }) {
   const isDroppingFeatures = useSelector((state) =>
     checkIfSourceIsDroppingFeature(state, dataSource)
   );
 
-  return (!global && isDroppingFeatures && showDroppingFeaturesAlert) ||
-    warning ||
-    !children ? (
+  const [childrenRef, setChildenRef] = useState();
+  const [savedHeight, setSavedHeight] = useState();
+  const noData =
+    (!global && isDroppingFeatures && showDroppingFeaturesAlert) || warning || !children;
+
+  if (stableHeight) {
+    if (noData && childrenRef && savedHeight === undefined) {
+      setSavedHeight(childrenRef?.clientHeight);
+    } else if (!noData && savedHeight !== undefined) {
+      setSavedHeight(undefined);
+    }
+  }
+
+  return noData ? (
     <NoDataAlert
       {...(isDroppingFeatures
         ? droppingFeaturesAlertProps
         : warning
         ? { ...noDataAlertProps, body: warning }
         : noDataAlertProps)}
+      style={{ height: savedHeight }}
     />
   ) : (
-    children
+    <div ref={setChildenRef}>{children}</div>
   );
 }
