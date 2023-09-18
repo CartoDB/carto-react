6a0cb91012a2e143b0aeed39f97150ae3fca6d24
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-18 19:01:15 +0200

    typings for TimeSeries widget

---
+++ packages/react-widgets/src/widgets/TimeSeriesWidget.d.ts
@@ -0,0 +1,45 @@
+import { AggregationTypes, GroupDateTypes } from '@carto/react-core';
+import { CommonWidgetProps, MonoColumnWidgetProps } from '../types';
+
+export interface TimeseriesWidgetSerie {
+  operation: AggregationTypes;
+  operationColumn: string;
+}
+
+export type TimeSeriesWidgetProps = {
+  operationColumn?: string;
+  series?: TimeseriesWidgetSerie[];
+
+  stepSize: GroupDateTypes;
+  stepMultiplier?: number;
+  stepSizeOptions?: string[];
+
+  splitByCategory?: string;
+  splitByCategoryLimit?: number;
+  splitByCategoryValues?: string[];
+
+  chartType?: string;
+  tooltip?: boolean;
+  tooltipFormatter?: Function;
+  formatter?: Function;
+
+  height?: string;
+  fitHeihgt?: boolean;
+  stableHeight?: boolean;
+  showControls?: boolean;
+  showLegend?: boolean;
+
+  isPlaying?: boolean;
+  isPaused?: boolean;
+  timeWindow?: number[];
+
+  onPlay?: () => void;
+  onPause?: () => void;
+  onStop?: () => void;
+  onTimelineUpdate?: (position: number) => void;
+  onTimeWindowUpdate?: (timeWindow: [number, number]) => void;
+} & CommonWidgetProps &
+  MonoColumnWidgetProps;
+
+declare const TimeSeriesWidget: (props: TimeSeriesWidgetProps) => JSX.Element;
+export default TimeSeriesWidget;
