48b58ff5d1c4e9c8c4c518ca9b727abe998e9266
menusal <daniarlandis@gmail.com>
2023-09-19 14:27:48 +0200

    Update imperative hook

--- packages/react-ui/src/hooks/useImperativeIntl.js
+++ packages/react-ui/src/hooks/useImperativeIntl.js
@@ -1,21 +1,17 @@
-import { IntlContext, createIntl, createIntlCache, useIntl } from 'react-intl';
+import { createIntl, createIntlCache, useIntl, IntlContext } from 'react-intl';
 import { messages } from '../localization';
 import { useContext, useMemo } from 'react';
 import { match } from '@formatjs/intl-localematcher';
+import { createContext } from 'react';
 
-export const DEFAULT_LOCALE = 'en-US';
+export const DEFAULT_LOCALE = 'en';
 const cache = createIntlCache();
 
-// TODO: Pending to get external intl provider
+export const I18nContext = createContext(IntlContext);
 
 export default function useImperativeIntl() {
-  const extenal = useIntl();
-  console.log('useImperativeIntl', extenal);
-  // TODO: External intl
-  const externalIntl = useContext(IntlContext);
-  console.log('findMatchingMessagesLocale', externalIntl);
-
-  const locale = externalIntl?.locale || DEFAULT_LOCALE;
+  const external = useContext(I18nContext);
+  const locale = external?.locale || DEFAULT_LOCALE;
   const messagesLocale = findMatchingMessagesLocale(locale);
 
   return useMemo(
