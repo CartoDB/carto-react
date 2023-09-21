70fddb476870f699fbe5287191028345508b090a
Zbigniew Zagórski <zbigniew@cartodb.com>
2023-09-21 09:52:49 +0200

    TimeSeriesWidget: fix clicking on line in single line mode

--- packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
+++ packages/react-ui/src/widgets/TimeSeriesWidgetUI/TimeSeriesWidgetUI.js
@@ -291,7 +291,7 @@ function TimeSeriesWidgetUIContent({
       fitHeight={fitHeight}
       animation={animation}
       selectedCategories={selectedCategories}
-      onCategoryClick={handleCategoryClick}
+      onCategoryClick={onSelectedCategoriesChange && handleCategoryClick}
     />
   );
 
