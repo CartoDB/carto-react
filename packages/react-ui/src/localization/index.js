80b169c6a7b616aa08b86439edf6f9cceb1bf2bd
menusal <daniarlandis@gmail.com>
2023-10-02 20:49:55 +0200

    remove non default languages

--- packages/react-ui/src/localization/index.js
+++ packages/react-ui/src/localization/index.js
@@ -1,12 +1,9 @@
 import en from './en';
-import es from './es';
 
 import { flattenMessages } from './localeUtils';
 
 const enFlat = flattenMessages(en);
-const esFlat = flattenMessages(es);
 
 export const messages = {
-  en: enFlat,
-  es: esFlat
+  en: enFlat
 };
