8275dcd0213a122441e608f008cb481eeceeae13
menusal <daniarlandis@gmail.com>
2023-10-19 14:32:15 +0200

    update hook to get external config

--- packages/react-ui/src/hooks/useImperativeIntl.js
+++ packages/react-ui/src/hooks/useImperativeIntl.js
@@ -1,35 +1,29 @@
 import { createIntl, createIntlCache } from 'react-intl';
 import { messages } from '../localization';
 import { useMemo } from 'react';
-import { match } from '@formatjs/intl-localematcher';
-import { flattenMessages } from '../localization/localeUtils';
+import {
+  flattenMessages,
+  findMatchingMessagesLocale,
+  DEFAULT_LOCALE
+} from '../localization/localeUtils';
 
-export const DEFAULT_LOCALE = 'en';
 const cache = createIntlCache();
 
 export default function useImperativeIntl(intlConfig) {
   return useMemo(() => {
     const locale = intlConfig?.locale || DEFAULT_LOCALE;
-    const messagesLocale = findMatchingMessagesLocale(locale);
-    const intMessages = intlConfig?.messages
-      ? flattenMessages(intlConfig?.messages)
-      : messages[messagesLocale];
+    const messagesLocale = findMatchingMessagesLocale(locale, messages);
+    const intMessages =
+      intlConfig?.messages && Object.keys(intlConfig?.messages).length > 0
+        ? intlConfig?.messages
+        : messages[messagesLocale];
 
-    return (
-      createIntl(
-        {
-          locale,
-          messages: intMessages
-        },
-        cache
-      ),
-      [locale, intMessages]
+    return createIntl(
+      {
+        locale,
+        messages: flattenMessages(intMessages)
+      },
+      cache
     );
   }, [intlConfig]);
-}
-
-// AUX
-function findMatchingMessagesLocale(locale) {
-  const localeMatcher = match([locale], Object.keys(messages), DEFAULT_LOCALE);
-  return localeMatcher ? localeMatcher : DEFAULT_LOCALE;
 }
