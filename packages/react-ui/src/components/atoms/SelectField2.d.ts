cac8a578a01837621205ebea8d24a64ef718f848
Verónica Milán <vmilan@cartodb.com>
2023-11-28 12:29:24 +0100

    fix types

--- packages/react-ui/src/components/atoms/SelectField2.d.ts
+++ packages/react-ui/src/components/atoms/SelectField2.d.ts
@@ -3,14 +3,13 @@ import { MenuProps } from '@mui/material';
 import { SelectProps } from '@mui/material/Select';
 import { TextFieldProps } from '@mui/material/TextField';
 
-export type SelectFieldProps2 = Omit<TextFieldProps, 'placeholder'> &
-  Omit<SelectProps, 'placeholder'> & {
-    placeholder?: React.ReactNode | string;
-    size?: 'small' | 'medium';
-    renderValue?: (value: string[]) => React.ReactNode;
-    menuProps?: Partial<MenuProps>;
-    helperText?: React.ReactNode | string;
-  };
+export type SelectFieldProps2 = Omit<SelectProps, 'placeholder'> & {
+  placeholder?: React.ReactNode | string;
+  size?: 'small' | 'medium';
+  renderValue?: (value: string[]) => React.ReactNode;
+  menuProps?: Partial<MenuProps>;
+  helperText?: React.ReactNode | string;
+};
 
 declare const SelectField2: (props: SelectFieldProps2) => JSX.Element;
 export default SelectField2;
