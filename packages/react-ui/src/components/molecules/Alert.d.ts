d1223dcd2c1cfea9082bfe0ea39b230570cc23d2
Verónica Milán <vmilan@cartodb.com>
2023-12-18 12:57:44 +0100

    Alert types for title

--- packages/react-ui/src/components/molecules/Alert.d.ts
+++ packages/react-ui/src/components/molecules/Alert.d.ts
@@ -1,13 +1,12 @@
 import { AlertProps as MuiAlertProps } from '@mui/material/Alert';
 
-export type AlertProps = Omit<MuiAlertProps, 'severity'> &
-  Omit<MuiAlertProps, 'title'> & {
-    content?: 'inline' | 'block';
-    severity?: CartoAlertSeverity;
-    open?: boolean;
-    title?: React.ReactNode | string;
-    isSticky?: boolean;
-  };
+export type AlertProps = Omit<MuiAlertProps, 'severity' | 'title'> & {
+  content?: 'inline' | 'block';
+  severity?: CartoAlertSeverity;
+  open?: boolean;
+  title?: React.ReactNode | string;
+  isSticky?: boolean;
+};
 
 export type CartoAlertSeverity = 'neutral' | 'info' | 'success' | 'warning' | 'error';
 
