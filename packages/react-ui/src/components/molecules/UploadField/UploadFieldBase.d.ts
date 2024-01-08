ce34c761e016ef7b25ee399c89b720f25436f9ad
Verónica Milán <vmilan@cartodb.com>
2024-01-08 17:29:56 +0100

    simplify UploadFieldBase

--- packages/react-ui/src/components/molecules/UploadField/UploadFieldBase.d.ts
+++ packages/react-ui/src/components/molecules/UploadField/UploadFieldBase.d.ts
@@ -2,22 +2,18 @@ import React, { Ref } from 'react';
 import { TextFieldProps } from '@mui/material/TextField';
 import { InputProps } from '@mui/material';
 
-export type UploadFieldBaseProps = Omit<TextFieldProps, 'onChange' | 'error'> & {
+export type UploadFieldBaseProps = Omit<TextFieldProps, 'error'> & {
   name?: string;
-  accept?: string[] | string | null;
   multiple?: boolean;
-  onChange?: (file?: File | null) => void;
   handleReset?: () => void;
   handleOpen?: () => void;
   dragOver?: boolean;
   error?: string | React.ReactNode;
-  inProgress?: boolean;
-  muiInputProps?: Partial<InputProps>;
-  nativeInputProps?: object;
-  size?: 'small' | 'medium';
   buttonText?: string;
-  filesText?: File | string | null;
-  inputRef?: Ref<any>;
+  inProgress?: boolean;
+  inputProps?: Partial<InputProps>;
+  size?: 'small' | 'medium';
+  hasFiles?: boolean;
 };
 
 declare const UploadFieldBase: (props: UploadFieldBaseProps) => JSX.Element;
--- packages/react-ui/src/components/molecules/UploadField/UploadFieldBase.js
+++ packages/react-ui/src/components/molecules/UploadField/UploadFieldBase.js
@@ -5,91 +5,66 @@ import FilesAction from './FilesAction';
 import StyledUploadField from './StyledUploadField';
 
 function UploadFieldBase({
-  buttonText,
-  inProgress,
-  size,
-  error,
-  filesText,
-  muiInputProps,
-  nativeInputProps,
-  onChange,
+  name,
+  multiple,
   handleReset,
   handleOpen,
   dragOver,
-  name,
-  accept,
-  inputRef,
-  multiple,
+  error,
   focused,
+  buttonText,
+  inProgress,
+  inputProps,
+  size,
+  hasFiles,
   ...props
 }) {
   return (
-    <>
-      <StyledUploadField
-        {...props}
-        size={size}
-        error={!!error}
-        helperText={error}
-        focused={focused || dragOver}
-        InputProps={{
-          ...muiInputProps,
-          readOnly: true,
-          multiple: multiple,
-          name: name,
-          endAdornment: (
-            <FilesAction
-              buttonText={buttonText}
-              hasFiles={!!filesText}
-              size={size}
-              error={!!error}
-              disabled={!!dragOver}
-              handleReset={handleReset}
-              handleOpen={handleOpen}
-              inProgress={inProgress}
-            />
-          )
-        }}
-      />
-      <input
-        {...nativeInputProps}
-        ref={inputRef}
-        style={{ display: 'none' }}
-        type='file'
-        aria-label={name}
-        name={name}
-        onChange={onChange}
-        accept={accept}
-      />
-    </>
+    <StyledUploadField
+      {...props}
+      size={size}
+      error={!!error}
+      helperText={error}
+      focused={focused || dragOver}
+      InputProps={{
+        ...inputProps,
+        readOnly: true,
+        multiple: multiple,
+        name: name,
+        endAdornment: (
+          <FilesAction
+            buttonText={buttonText}
+            hasFiles={hasFiles}
+            size={size}
+            error={!!error}
+            disabled={!!dragOver}
+            handleReset={handleReset}
+            handleOpen={handleOpen}
+            inProgress={inProgress}
+          />
+        )
+      }}
+    />
   );
 }
 
 UploadFieldBase.defaultProps = {
   buttonText: 'Browse',
-  filesText: null,
-  size: 'small',
-  onChange: (files) => files
+  size: 'small'
 };
 
 UploadFieldBase.propTypes = {
   name: PropTypes.string,
-  accept: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.string), PropTypes.string]),
   multiple: PropTypes.bool,
-  onChange: PropTypes.func,
   handleReset: PropTypes.func,
   handleOpen: PropTypes.func,
   dragOver: PropTypes.bool,
   error: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-  inProgress: PropTypes.bool,
-  muiInputProps: PropTypes.object,
-  nativeInputProps: PropTypes.object,
-  size: PropTypes.oneOf(['small', 'medium']),
   buttonText: PropTypes.string,
-  filesText: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(File)]),
-  inputRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.instanceOf(Element) })
-  ])
+  inProgress: PropTypes.bool,
+  inputProps: PropTypes.object,
+  size: PropTypes.oneOf(['small', 'medium']),
+  hasFiles: PropTypes.bool
 };
 
 export default UploadFieldBase;
