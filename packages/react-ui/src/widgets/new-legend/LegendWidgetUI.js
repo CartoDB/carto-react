b34565033d8cea69a83bf5d93f333562361437ef
Juan D. Jara <juanorigami@gmail.com>
2024-02-02 15:29:13 +0100

    finish icon buttons with tooltips

--- packages/react-ui/src/widgets/new-legend/LegendWidgetUI.js
+++ packages/react-ui/src/widgets/new-legend/LegendWidgetUI.js
@@ -8,6 +8,7 @@ import {
   Popover,
   Slider,
   TextField,
+  Tooltip,
   Typography
 } from '@mui/material';
 import { LEGEND_TYPES, LegendCategories, LegendIcon, LegendRamp } from '@carto/react-ui';
@@ -56,7 +57,8 @@ export const styles = {
     gap: 0.5,
     display: 'flex',
     justifyContent: 'space-between',
-    position: 'relative'
+    position: 'sticky',
+    top: 0
   },
   legendItemBody: {
     px: 2
@@ -85,7 +87,7 @@ export const styles = {
     gap: 2,
     alignItems: 'center',
     p: 1,
-    width: 208
+    minWidth: 208
   },
   layerOptions: {
     background: (theme) => theme.palette.background.default,
@@ -164,9 +166,11 @@ function NewLegendWidgetUI({
   return (
     <Paper elevation={3} sx={rootSx}>
       {collapsed ? (
-        <IconButton onClick={() => onChangeCollapsed(false)}>
-          <LayerIcon />
-        </IconButton>
+        <Tooltip title='Open legend'>
+          <IconButton onClick={() => onChangeCollapsed(false)}>
+            <LayerIcon />
+          </IconButton>
+        </Tooltip>
       ) : (
         <Box
           sx={{
@@ -177,9 +181,11 @@ function NewLegendWidgetUI({
           <Typography variant='caption' sx={{ flexGrow: 1 }}>
             {title}
           </Typography>
-          <IconButton size='small' onClick={() => onChangeCollapsed(true)}>
-            <CloseIcon />
-          </IconButton>
+          <Tooltip title='Close'>
+            <IconButton size='small' onClick={() => onChangeCollapsed(true)}>
+              <CloseIcon />
+            </IconButton>
+          </Tooltip>
         </Box>
       )}
       <Box sx={{ ...styles.legendItemList, width: collapsed ? 0 : undefined }}>
@@ -317,18 +323,20 @@ export function LegendItem({
           />
         )}
         {switchable && (
-          <IconButton
-            size='small'
-            onClick={() =>
-              onChangeVisibility({
-                id,
-                collapsed: visible ? collapsed : false,
-                visible: !visible
-              })
-            }
-          >
-            {visible ? <EyeIcon /> : <EyeOffIcon />}
-          </IconButton>
+          <Tooltip title={visible ? 'Hide layer' : 'Show layer'}>
+            <IconButton
+              size='small'
+              onClick={() =>
+                onChangeVisibility({
+                  id,
+                  collapsed: visible ? collapsed : false,
+                  visible: !visible
+                })
+              }
+            >
+              {visible ? <EyeIcon /> : <EyeOffIcon />}
+            </IconButton>
+          </Tooltip>
         )}
       </Box>
       <Collapse timeout={100} sx={styles.legendItemBody} in={isExpanded}>
@@ -360,39 +368,45 @@ function OpacityControl({ menuRef, open, toggleOpen, opacity, onChange }) {
 
   return (
     <>
-      <IconButton
-        size='small'
-        aria-label='Toggle opacity control'
-        color={open ? 'primary' : 'default'}
-        onClick={() => toggleOpen(!open)}
-      >
-        <svg
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
+      <Tooltip title='Layer opacity'>
+        <IconButton
+          size='small'
+          aria-label='Toggle opacity control'
+          color={open ? 'primary' : 'default'}
+          onClick={() => toggleOpen(!open)}
         >
-          <path
-            fillRule='evenodd'
-            clipRule='evenodd'
-            d='M19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3ZM17.625 19V17.625H19V15.375H17.625V13.125H19V10.875H17.625V8.625H19V6.375H17.625V5H15.375V6.375H13.125V5H10.875V6.375H8.625V5H6.375V6.375H5V8.625H6.375V10.875H5V13.125H6.375V15.375H5V17.625H6.375V19H8.625V17.625H10.875V19H13.125V17.625H15.375V19H17.625ZM15.375 15.375H17.625V17.625H15.375V15.375ZM13.125 15.375H15.375V13.125H17.625V10.875H15.375V8.625H17.625V6.375H15.375V8.625H13.125V6.375H10.875V8.625H8.625V6.375H6.375V8.625H8.625V10.875H6.375V13.125H8.625V15.375H6.375V17.625H8.625V15.375H10.875V17.625H13.125V15.375ZM13.125 13.125H15.375V10.875H13.125V8.625H10.875V10.875H8.625V13.125H10.875V15.375H13.125V13.125ZM13.125 13.125H10.875V10.875H13.125V13.125Z'
-          />
-        </svg>
-      </IconButton>
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3ZM17.625 19V17.625H19V15.375H17.625V13.125H19V10.875H17.625V8.625H19V6.375H17.625V5H15.375V6.375H13.125V5H10.875V6.375H8.625V5H6.375V6.375H5V8.625H6.375V10.875H5V13.125H6.375V15.375H5V17.625H6.375V19H8.625V17.625H10.875V19H13.125V17.625H15.375V19H17.625ZM15.375 15.375H17.625V17.625H15.375V15.375ZM13.125 15.375H15.375V13.125H17.625V10.875H15.375V8.625H17.625V6.375H15.375V8.625H13.125V6.375H10.875V8.625H8.625V6.375H6.375V8.625H8.625V10.875H6.375V13.125H8.625V15.375H6.375V17.625H8.625V15.375H10.875V17.625H13.125V15.375ZM13.125 13.125H15.375V10.875H13.125V8.625H10.875V10.875H8.625V13.125H10.875V15.375H13.125V13.125ZM13.125 13.125H10.875V10.875H13.125V13.125Z'
+            />
+          </svg>
+        </IconButton>
+      </Tooltip>
       <Popover
         open={open}
         onClose={() => toggleOpen(false)}
         anchorEl={menuRef.current}
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center'
+          vertical: 'top',
+          horizontal: 'right'
         }}
         transformOrigin={{
           vertical: 'top',
-          horizontal: 'center'
+          horizontal: 'right'
         }}
-        sx={{ marginTop: -2 }}
+        slotProps={{
+          root: {
+            sx: { transform: 'translate(-12px, 36px)' }
+          }
+        }}
       >
         <Box sx={styles.opacityControl}>
           <Slider
