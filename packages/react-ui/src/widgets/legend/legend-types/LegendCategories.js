9e0e82290c36155a7e01acfce54e95712282d14a
padawannn <jaragon@cartodb.com>
2024-05-29 09:34:04 +0200

    fix legend deformed dot when the title is too long

--- packages/react-ui/src/widgets/legend/legend-types/LegendCategories.js
+++ packages/react-ui/src/widgets/legend/legend-types/LegendCategories.js
@@ -132,6 +132,7 @@ const Marker = styled(Box, {
   display: 'block',
   width: theme.spacing(1.5),
   height: theme.spacing(1.5),
+  flex: '0, 0, auto',
   borderRadius: '50%',
   position: 'relative',
   border: '2px solid transparent',
