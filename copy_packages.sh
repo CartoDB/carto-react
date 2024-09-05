bda4681451566dbb74ac3d142563c948a5000bc0
Julio Muñoz <jmunoz@cartodb.com>
2024-09-05 13:14:24 +0200

    Add support for geoColumn parameter in TableWidget

---
+++ copy_packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Missing target directory. Usage: $0 <target_dir>"
+  exit 1
+fi
+
+SOURCE_DIR="packages"
+TARGET_DIR="$1"
+
+for dir in "$SOURCE_DIR"/*; do
+  if [ -d "$dir" ]; then
+    DIR_NAME=$(basename "$dir")
+    
+    SOURCE_PATH="$dir/dist"
+    TARGET_PATH="$TARGET_DIR/$DIR_NAME"
+    
+    echo "Copying $SOURCE_PATH to $TARGET_PATH"
+    cp -r "$SOURCE_PATH" "$TARGET_PATH"
+  fi
+done
+
+echo "All directories copied successfully!"
