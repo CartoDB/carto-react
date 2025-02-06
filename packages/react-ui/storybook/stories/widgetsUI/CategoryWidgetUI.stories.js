e43522747ebfba90fef4e2eb6eecb3e7f7e1826a
Julio Muñoz <jmunoz@cartodb.com>
2025-02-06 11:01:27 +0100

    Simplify Storybook, so we can display up to 200K elements

--- packages/react-ui/storybook/stories/widgetsUI/CategoryWidgetUI.stories.js
+++ packages/react-ui/storybook/stories/widgetsUI/CategoryWidgetUI.stories.js
@@ -1,33 +1,10 @@
 import React from 'react';
 import CategoryWidgetUI from '../../../src/widgets/CategoryWidgetUI/CategoryWidgetUI';
-import { Label, ThinContainer } from '../../utils/storyStyles';
 import { IntlProvider } from 'react-intl';
 
 const options = {
   title: 'Widgets/CategoryWidgetUI',
-  component: CategoryWidgetUI,
-  argTypes: {
-    selectedCategories: {
-      table: { disable: true }
-    },
-    onSelectedCategoriesChange: {
-      table: { disable: true }
-    },
-    order: {
-      defaultValue: 'ranking',
-      control: {
-        type: 'select',
-        options: ['ranking', 'fixed']
-      }
-    }
-  },
-  parameters: {
-    docs: {
-      source: {
-        type: 'auto'
-      }
-    }
-  }
+  component: CategoryWidgetUI
 };
 
 export default options;
@@ -42,63 +19,15 @@ const Template = (args) => {
   return <Widget {...args} />;
 };
 
-const LoadingTemplate = (args) => {
-  return (
-    <>
-      <Label variant='body1' mb={3}>
-        {'Limited width'}
-      </Label>
-      <ThinContainer>
-        <Widget {...args} />
-      </ThinContainer>
-
-      <Label variant='body1' mt={8} mb={3}>
-        {'Responsive'}
-      </Label>
-      <Widget {...args} />
-    </>
-  );
-};
-
-const data = [...Array(2000)].map((_, idx) => ({
+const data = [...Array(200000)].map((_, idx) => ({
   name: `Category ${idx + 1}`,
   value: idx * 100
 }));
 
-const filteredData = data.slice(0, 4).map((cat, idx) => {
-  return { ...cat, value: idx % 2 === 0 ? null : cat.value };
-});
-
 export const Default = Template.bind({});
-const DefaultProps = { data };
-Default.args = DefaultProps;
-
-export const OnlyData = Template.bind({});
-OnlyData.args = DefaultProps;
-
-export const WithFormatter = Template.bind({});
-WithFormatter.args = DefaultProps;
-
-export const WithCustomLabels = Template.bind({});
-const WithCustomLabelsProps = {
-  ...DefaultProps,
-  labels: {
-    'Category 1': 'Cat. 1',
-    'Category 2': 'Cat. 2',
-    'Category 3': 'Cat. 3',
-    'Category 4': 'Cat. 4'
+Default.args = { data };
+Default.parameters = {
+  controls: {
+    exclude: ['data']
   }
 };
-WithCustomLabels.args = WithCustomLabelsProps;
-
-export const WithSelectedCategories = Template.bind({});
-const WithSelectedCategoriesProps = {
-  data: filteredData,
-  selectedCategories: ['Category 2', 'Category 4'],
-  onSelectedCategoriesChange: (categories) => console.log(categories)
-};
-WithSelectedCategories.args = WithSelectedCategoriesProps;
-
-export const Loading = LoadingTemplate.bind({});
-const LoadingProps = { data, isLoading: true };
-Loading.args = LoadingProps;
