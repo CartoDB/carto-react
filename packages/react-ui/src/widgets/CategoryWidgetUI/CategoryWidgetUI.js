8a53bc291a5aaa02667bb31be2c7c73b3ad4b0d1
Julio Muñoz <jmunoz@cartodb.com>
2025-02-06 13:48:55 +0100

    Refine item height (again)

--- packages/react-ui/src/widgets/CategoryWidgetUI/CategoryWidgetUI.js
+++ packages/react-ui/src/widgets/CategoryWidgetUI/CategoryWidgetUI.js
@@ -537,10 +537,10 @@ function CategoryWidgetUI(props) {
       <CategoriesWrapper container item>
         {animValues.length ? (
           <FixedSizeList
-            height={!showAll ? (maxItems + 1) * 36 : 320}
+            height={!showAll ? (maxItems + 1) * 38 : 320}
             width='100%'
             itemCount={animValues.length}
-            itemSize={32}
+            itemSize={36}
           >
             {CategoryRow}
           </FixedSizeList>
